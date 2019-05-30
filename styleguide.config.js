098cffe0f0eea35bf0cac87db30d60ba27c65f2a
Dhruv Bhanushali <dhruv_b@live.com>
2019-05-31 00:21:55 +0530

    Update stylesheets to include the light version of Source Sans Pro

--- styleguide.config.js
+++ styleguide.config.js
@@ -40,7 +40,7 @@ module.exports = {
         },
         {
           rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700&display=swap'
+          href: 'https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700&display=swap'
         },
         {
           rel: 'icon',
