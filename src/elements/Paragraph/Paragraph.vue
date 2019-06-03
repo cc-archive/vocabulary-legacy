78883206de5324d94a92207c584131c04f1735b7
Dhruv Bhanushali <dhruv_b@live.com>
2019-06-03 11:21:16 +0530

    Add an element Paragraph that provides some utilities over the standard `p` tag.

---
+++ src/elements/Paragraph/Paragraph.vue
@@ -0,0 +1,24 @@
+<template>
+  <p :class="{inverted: inverted}">
+    <!-- @slot Content goes here -->
+    <slot/>
+  </p>
+</template>
+
+<script>
+  export default {
+    name: 'Paragraph',
+    props: {
+      /**
+       * whether the content is on a dark background
+       */
+      inverted: {
+        type: Boolean,
+        default: false
+      }
+    }
+  }
+</script>
+
+<style scoped lang="stylus" src="./Paragraph.styl">
+</style>
