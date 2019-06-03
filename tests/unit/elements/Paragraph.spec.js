b8270574868a3daa94df1be05fbe0d76eb862808
Dhruv Bhanushali <dhruv_b@live.com>
2019-06-03 11:32:53 +0530

    Improve and add tests

--- tests/unit/elements/Paragraph.spec.js
+++ tests/unit/elements/Paragraph.spec.js
@@ -1,17 +1,15 @@
 import { shallowMount } from '@vue/test-utils'
 
-import Heading from '@/elements/Heading/Heading'
+import Paragraph from '@/elements/Paragraph/Paragraph'
 
-describe('Heading.vue', () => {
+describe('Paragraph.vue', () => {
   let propsData, slots, options
 
   beforeEach(() => {
-    let level = 1
-    let slot = '<span>Slot</span>'
     let inverted = true
+    let slot = '<span>Slot</span>'
 
     propsData = {
-      level,
       inverted
     }
     slots = {
@@ -24,25 +22,25 @@ describe('Heading.vue', () => {
   })
 
   it('renders correct tag', () => {
-    const wrapper = shallowMount(Heading, options)
+    const wrapper = shallowMount(Paragraph, options)
 
-    expect(wrapper.is('h1')).toBe(true)
+    expect(wrapper.is('p')).toBe(true)
   })
 
   it('renders correct style', () => {
-    const wrapper = shallowMount(Heading, options)
+    const wrapper = shallowMount(Paragraph, options)
 
     expect(wrapper.element.classList).toContain('inverted')
   })
 
   it('renders correct slot', () => {
-    const wrapper = shallowMount(Heading, options)
+    const wrapper = shallowMount(Paragraph, options)
 
     expect(wrapper.find('span').exists()).toBe(true)
   })
 
   it('renders correct text', () => {
-    const wrapper = shallowMount(Heading, options)
+    const wrapper = shallowMount(Paragraph, options)
 
     expect(wrapper.text()).toEqual('Slot')
   })
