8ed575bac8227446a1de7a911e10c39c06c4d065
Dhruv Bhanushali <dhruv_b@live.com>
2019-07-02 14:49:02 +0530

    Define the indent rule for switch-case blocks

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'vue/script-indent': ['error', 2, {
+      switchCase: 1,
       baseIndent: 1
     }] // Supersedes the normal indent rule
   },
