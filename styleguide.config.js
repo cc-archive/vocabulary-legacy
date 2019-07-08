e89ca1529a0f74d138b18d15bbff39ae04dfe94f
Dhruv Bhanushali <dhruv_b@live.com>
2019-07-08 13:47:09 +0530

    Make Styleguide play nicely on newer curved displays

--- styleguide.config.js
+++ styleguide.config.js
@@ -47,6 +47,12 @@ module.exports = {
   ],
   template: {
     head: {
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1.0, viewport-fit=cover'
+        }
+      ],
       links: [
         {
           rel: 'stylesheet',
