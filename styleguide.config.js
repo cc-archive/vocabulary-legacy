342807cbdeb8687938e4c7ed8397282eeba3ebcd
Dhruv Bhanushali <dhruv_b@live.com>
2019-07-17 06:15:09 +0530

    Make portions of the site mobile responsive

--- styleguide.config.js
+++ styleguide.config.js
@@ -58,7 +58,7 @@ module.exports = {
       meta: [
         {
           name: 'viewport',
-          content: 'width=device-width, initial-scale=1.0, viewport-fit=cover'
+          content: 'width=device-width, initial-scale=1.0, user-scalable=no, viewport-fit=cover'
         }
       ],
       links: [
