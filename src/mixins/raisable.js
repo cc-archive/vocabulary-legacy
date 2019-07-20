ee9e808ab70e285b925d13bb6ae2ed6276d0cb4f
Dhruv Bhanushali <dhruv_b@live.com>
2019-07-21 04:08:24 +0530

    Define a mixin for making raisable components

---
+++ src/mixins/raisable.js
@@ -0,0 +1,11 @@
+export default {
+  props: {
+    /**
+     * _whether the component should appear raised by means of a shadow_
+     */
+    isRaised: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
