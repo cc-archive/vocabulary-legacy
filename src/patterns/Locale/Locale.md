06ca9a4f001ed08668347f22e4e1c56790a73483
Dhruv Bhanushali <dhruv_b@live.com>
2019-08-06 01:46:21 +0530

    Refactor code according to the style guide and rewrite updated documentation

--- src/patterns/Locale/Locale.md
+++ src/patterns/Locale/Locale.md
@@ -1,21 +1,53 @@
-## Examples
-
 The locale switcher in the standard form looks like this.
 
 ```jsx
 <Locale/>
 ``` 
 
+### Locale set
+
+By default Vocabulary ships with some locales. But these can be replaced using
+the `localeList` prop.
+
+```jsx
+let localeList = [
+  {
+    code: 'hi',
+    englishName: 'Hindi', // For the love of God,
+    nativeName: 'Hindi'   // do not subject your users to this.
+  },
+  {
+    code: 'en',
+    englishName: 'अंग्रेज़ी',  // For the love of God,
+    nativeName: 'अंग्रेज़ी'    // do not subject your users to this.
+  }
+];
+
+<Locale :locale-list="localeList"/>
+```
+
 In components that have i18n enabled and translations defined this component
 appears in the preview as well, allowing explorers (like yourself!) to change 
 locales and see the various versions of the component.
 
 ```jsx { "props": { "className": "i18n-enabled" } }
+<Hello/>
+```
+
+You can embed the switcher in any component inside any Vue i18n configured
+application (which is a must for running CC Vocabulary) and it will enable
+toggling between the translations for that application. The
+[Footer](#/Patterns/Footer) is an ideal place.
+
+```jsx
 <Footer>
   <Locale/>
 </Footer>
 ```
 
-All locale switchers across the styleguide, including those in component 
-previews, share the same scope and thus switching one will cause a switch in all
-components across the styleguide.
+All locale switchers across the documentation (or any application for that
+matter), including those in component previews, are reactively bound to the same
+variable via `v-model` and thus switching one will cause a switch in all
+components everywhere.
+
+Pretty powerful stuff!
--- src/patterns/Locale/Locale.vue
+++ src/patterns/Locale/Locale.vue
@@ -5,20 +5,20 @@
       v-bind="$attrs"
       color="orange"
       icon="globe"
-      :optionList="localeOptions"/>
+      :option-list="localeOptions"/>
   </div>
 </template>
 
 <script>
-  import SelectField from '@/elements/SelectField/SelectField'
-
   import { library } from '@fortawesome/fontawesome-svg-core'
   import { faGlobe } from '@fortawesome/free-solid-svg-icons'
+
+  import SelectField from '@/elements/SelectField/SelectField'
 
   library.add(faGlobe)
 
   /**
-   * ## Locale translates Vocabulary to the users' language.
+   * ### Locale brings the content to you.
    *
    * When exploring an app that has translations defined, the user can
    * switch between locales using this component.
@@ -29,20 +29,46 @@
     components: {
       SelectField
     },
+    props: {
+      /**
+       * _an array of locale objects defining the app's supported locales_
+       *
+       * Each object must contain `code`, `englishName` and `nativeName`.
+       *
+       * ```js
+       * let localeList = [
+       *   {
+       *     code: 'fr',
+       *     englishName: 'French',
+       *     nativeName: 'le français'
+       *   }
+       * ]
+       * ```
+       */
+      localeList: {
+        type: Array,
+        validator: val => val.every(
+          locale => ['code', 'englishName', 'nativeName'].every(
+            key => key in locale
+          )
+        )
+      }
+    },
     data: function () {
+      let defaultLocales = [
+        {
+          code: 'hi',
+          englishName: 'Hindi',
+          nativeName: 'हिन्दी'
+        },
+        {
+          code: 'en',
+          englishName: 'English',
+          nativeName: 'English'
+        }
+      ]
       return {
-        locales: [
-          {
-            code: 'hi',
-            englishName: 'Hindi',
-            nativeName: 'हिन्दी'
-          },
-          {
-            code: 'en',
-            englishName: 'English',
-            nativeName: 'English'
-          }
-        ]
+        locales: this.localeList || defaultLocales
       }
     },
     computed: {
