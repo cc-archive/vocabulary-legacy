a08262f3aacfba8b0a9e61ddc9e0a4bab8638bec
Dhruv Bhanushali <dhruv_b@live.com>
2019-08-06 12:50:29 +0530

    Clarify the purpose of the mixin Invertible

--- src/mixins/invertible.js
+++ src/mixins/invertible.js
@@ -3,7 +3,7 @@ export default {
     /**
      * _whether the component appears on a dark or non-white background_
      *
-     * This essentially overrides the color, setting it to `'white'`.
+     * This essentially negates all greyscale colors.
      */
     isInverted: {
       type: Boolean,
