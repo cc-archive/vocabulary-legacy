b4b25e58e9d215f5f2f8cb4ca6c8582c046c63de
Dhruv Bhanushali <dhruv_b@live.com>
2019-08-11 05:04:13 +0530

    Improve the functionality of the collapsing sidebar and weed out bugs

--- styleguidist/script.js
+++ styleguidist/script.js
@@ -1,56 +1,101 @@
-let navLinkList
-let navLinkMap = {}
-let navLinks
-let activeLink
 let searchField
 
+let navLinkList
+let navLinks
+
+let navLinkMap = {}
+
+let activeLink
+let highlightedLink
+
+// Map active hash to classes on navigation tree
+
 function hashToNav () {
+  let hash = window.location.hash
+  let parts = hash.split('/')
+
+  let family = parts[1]
+  if (family) {
+    family = family.replace('%20', ' ')
+  } else {
+    family = 'Vocabulary'
+  }
+  let member = parts[2]
+  if (member) {
+    member = member.replace('%20', ' ')
+  }
+
   mapLinks()
 
-  let oldActiveLink, newActiveLink
-
-  oldActiveLink = activeLink
-  if (oldActiveLink) {
-    oldActiveLink.classList.remove('cc-active')
+  if (activeLink) {
+    activeLink.classList.remove('cc-active')
+  }
+  if (highlightedLink) {
+    highlightedLink.classList.remove('cc-highlighted')
   }
 
-  if (window.location.hash === '') {
-    newActiveLink = navLinks[0]
-  } else {
-    let hash = window.location.hash
-    let componentFamily = hash.split('/')[1]
-    newActiveLink = navLinkMap[componentFamily]
-  }
-  newActiveLink.classList.add('cc-active')
+  activeLink = navLinkMap[family].domObject
+  activeLink.classList.add('cc-active')
 
-  activeLink = newActiveLink
+  if (member) {
+    highlightedLink = navLinkMap[family].children[member].domObject
+    highlightedLink.classList.add('cc-highlighted')
+  }
 }
 
+// Convert navigation tree to JavaScript object
+
+function mapLinks () {
+  navLinkList = document.querySelector(
+    'div[class*="rsg--sidebar"] nav > ul'
+  )
+
+  navLinks = navLinkList.children
+  navLinks.forEach(navLink => {
+    let innerText = navLink.querySelector('a').innerText
+    navLinkMap[innerText] = {
+      domObject: navLink,
+      children: Array.from(navLink.querySelector('ul').children).reduce(
+        (obj, item) => {
+          obj[item.innerText] = {
+            domObject: item
+          }
+          return obj
+        },
+        {}
+      )
+    }
+  })
+}
+
+// Filter field focus and blur events
+
 function filterOn () {
-  navLinkList.classList.add('cc-filtering')
+  navLinkList.classList.remove('cc-collapsing')
+  activeLink.classList.remove('cc-active')
+  highlightedLink.classList.remove('cc-highlighted')
 }
 
 function filterOff () {
   if (!searchField.value) {
-    navLinkList.classList.remove('cc-filtering')
     hashToNav()
+    navLinkList.classList.add('cc-collapsing')
   }
 }
 
-function mapLinks () {
-  navLinkList = document.querySelector('div[class*="rsg--sidebar"] nav > ul')
-  navLinks = navLinkList.children
-  navLinks.forEach(navLink => {
-    navLinkMap[navLink.innerText] = navLink
-  })
-}
+// Attach listeners to window events
 
 window.onload = () => {
   hashToNav()
+  navLinkList.classList.add('cc-collapsing')
 
-  searchField = document.querySelector('input[placeholder="Filter by name"')
+  searchField = document.querySelector(
+    'input[placeholder="Filter by name"'
+  )
+
+  // Attach listeners to search field focus and blur events
+
   searchField.onfocus = filterOn
   searchField.onblur = filterOff
 }
-
 window.onhashchange = hashToNav
