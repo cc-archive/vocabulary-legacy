13fb721447336f1b859ddd34e075e7fcba2bc2a9
Dhruv Bhanushali <dhruv_b@live.com>
2019-12-31 15:04:18 +0530

    Enable i18n in the Storybook

--- storybook/config.js
+++ storybook/config.js
@@ -2,6 +2,8 @@ import { addDecorator, addParameters, configure } from '@storybook/vue'
 import { withA11y } from '@storybook/addon-a11y'
 import { withKnobs } from '@storybook/addon-knobs'
 
+import i18n from '@/i18n'
+
 import vocabularyTheme from './theme'
 
 // Customise storybook
@@ -16,6 +18,16 @@ addParameters({
 
 addDecorator(withA11y)
 addDecorator(withKnobs)
+addDecorator(
+  () => ({
+    i18n,
+    template: '<story/>',
+    // https://github.com/storybookjs/storybook/issues/6548#issuecomment-504336665
+    beforeCreate: function () {
+      this.$root._i18n = this.$i18n
+    }
+  })
+)
 
 // Register stories
 
