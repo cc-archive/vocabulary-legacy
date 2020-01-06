decb228491073bd5897124fb81907a6edc11fa1e
Dhruv Bhanushali <dhruv_b@live.com>
2020-01-06 11:34:19 +0530

    Rearrange addons to make Knobs the default pane

--- storybook/addons.js
+++ storybook/addons.js
@@ -1,7 +1,10 @@
-import '@storybook/addon-a11y/register'
-import '@storybook/addon-actions/register'
-import '@storybook/addon-backgrounds/register'
-// import '@storybook/addon-docs/register'
+// Addons pane
 import '@storybook/addon-knobs/register'
-import '@storybook/addon-links/register'
+import '@storybook/addon-actions/register'
+import '@storybook/addon-a11y/register'
+
+// Demo pane
+import '@storybook/addon-backgrounds/register'
 import '@storybook/addon-viewport/register'
+
+import '@storybook/addon-links/register'
