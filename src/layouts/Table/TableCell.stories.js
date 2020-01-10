a2a09342f661c3261cb01a8c5183e6ddc172d989
Dhruv Bhanushali <dhruv_b@live.com>
2020-01-10 06:23:28 +0530

    Restore interactivity to the TableCell story

--- src/layouts/Table/TableCell.stories.js
+++ src/layouts/Table/TableCell.stories.js
@@ -8,30 +8,14 @@ export default { title: 'Layouts|TableCell' }
 const content = `
   <template #head>
     <tr>
-      <TableCell is-heading>Item</TableCell>
-      <TableCell is-heading>Taste</TableCell>
+      <TableCell is-heading>Prop</TableCell>
+      <TableCell is-heading>Effect</TableCell>
     </tr>
   </template>
   <tr>
-    <TableCell is-heading label="Item:">Mango</TableCell>
-    <TableCell label="Taste:" indication="positive">Sweetest</TableCell>
-  </tr>
-  <tr>
-    <TableCell is-heading label="Item:">Grapes</TableCell>
-    <TableCell label="Taste:" indication="positive">Sweet</TableCell>
-  </tr>
-  <tr>
-    <TableCell is-heading label="Item:">Apple</TableCell>
-    <TableCell label="Taste:" indication="probably">Meh</TableCell>
-  </tr>
-  <tr>
-    <TableCell is-heading label="Item:">Orange</TableCell>
-    <TableCell label="Taste:" indication="negative">Sour</TableCell>
-  </tr>
-  <tr>
-    <TableCell is-heading label="Item:">Lemon</TableCell>
-    <TableCell label="Taste:" indication="negative">Sourest</TableCell>
-  </tr>
+    <TableCell is-heading label="Prop:">Indication</TableCell>
+    <TableCell label="Effect:" :indication="indication">{{ indication }}</TableCell>
+</tr>
 `
 
 export const indicating = () => ({
