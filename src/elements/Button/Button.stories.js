ab361f4258864a4e32bfad8e8d334d84a166c4fc
Breno Ferreira <bferreira@windowslive.com>
2020-01-14 09:53:56 -0300

    Button storybook

---
+++ src/elements/Button/Button.stories.js
@@ -0,0 +1,13 @@
+export default { title: 'Elements|Button' }
+
+export const Button = () => '<button class="vocab button big">Button</button>'
+
+export const BigPrimary = () => '<button class="vocab button primary big">Button</button>'
+
+export const MediumPrimary = () => '<button class="vocab button primary medium">Button</button>'
+
+export const Action = () => '<button class="vocab button action small">Button</button>'
+
+export const TagWhite = () => '<button class="vocab button tag-white">Button</button>'
+
+export const TagGray = () => '<button class="vocab button tag-gray">Button</button>'
