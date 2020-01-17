6f0f15c5eb63dd2201db43091e1728aa51ee717b
Dhruv Bhanushali <dhruv_b@live.com>
2020-01-17 06:12:34 +0530

    Add variables required to build the storybook

--- documentation/variables.js
+++ documentation/variables.js
@@ -8,9 +8,12 @@ const rootDir = path.resolve(__dirname, '..')
 
 const srcDir = path.resolve(rootDir, 'src')
 const docsDir = path.resolve(rootDir, 'docs')
+const storybookDir = path.resolve(docsDir, 'storybook')
+
 
 module.exports = {
   verboseName,
   srcDir,
-  docsDir
+  docsDir,
+  storybookDir
 }
