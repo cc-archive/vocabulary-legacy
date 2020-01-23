341de8fc87e8e807b1c4681a51f57c2e3617b0fc
Neel Shah <neel12@somaiya.edu>
2020-01-23 15:02:31 -0800

    Sanitize path variables to accommodate spaces

--- documentation/variables.js
+++ documentation/variables.js
@@ -6,9 +6,24 @@ const verboseName = packageConfig.verboseName
 
 const rootDir = path.resolve(__dirname, '..')
 
-const srcDir = path.resolve(rootDir, 'src')
-const docsDir = path.resolve(rootDir, 'docs')
-const storybookDir = path.resolve(docsDir, 'storybook')
+let srcDir = path.resolve(rootDir, 'src')
+let docsDir = path.resolve(rootDir, 'docs')
+let storybookDir = path.resolve(docsDir, 'storybook')
+
+const sanitizePath = (path) => {
+  if(process.platform != 'win32'){
+    let tokens = path.split("/")
+    tokens = tokens.map(token => token.replace(/(\s+)/g, '\\$1'))
+    return tokens.join("/")
+  }
+  else{
+    return path
+  }
+}
+
+srcDir = sanitizePath(srcDir)
+docsDir = sanitizePath(docsDir)
+storybookDir = sanitizePath(storybookDir)
 
 module.exports = {
   verboseName,
