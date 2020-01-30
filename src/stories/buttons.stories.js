1931e18889247044c31911f6799e6cb6b7205bee
Breno Ferreira <bferreira@windowslive.com>
2020-01-30 10:40:30 -0300

    Migrate to bulma vocabulary

---
+++ src/stories/buttons.stories.js
@@ -0,0 +1,15 @@
+export default { title: 'Buttons' };
+
+export const Button = () => '<button class="button big">Button</button>';
+
+export const ButtonPrimary = () => '<button class="button is-primary big">Button</button>';
+
+export const ButtonInfo = () => '<button class="button is-info">Button</button>';
+
+export const ButtonDonate = () => '<button class="button donate medium">Button</button>';
+
+export const ButtonSuccess = () => '<button class="button is-success">Button</button>';
+
+export const ButtonWarning = () => '<button class="button is-warning">Button</button>';
+
+export const ButtonError = () => '<button class="button is-danger">Button</button>';
