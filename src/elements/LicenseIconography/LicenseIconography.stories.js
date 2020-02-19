6d536782b1bbd96e65f630178579679f3ae8daa3
dhruvi16 <dhruvibutti9477@gmail.com>
2020-02-19 11:28:55 +0530

    Break the icons into separate stories

--- src/elements/LicenseIconography/LicenseIconography.stories.js
+++ src/elements/LicenseIconography/LicenseIconography.stories.js
@@ -7,37 +7,93 @@ export default {
   ]
 }
 
-export const creativeCommons = () => ({
+export const CC = () => ({
   components: { LicenseIconography },
   template: `
     <LicenseIconography :icon-list="['']"/>
   `
 })
 
-export const licenseAspects = () => ({
+export const BY = () => ({
   components: { LicenseIconography },
   template: `
-    <LicenseIconography :icon-list="['by', 'nc', 'nd', 'sa']"/>
+    <LicenseIconography :icon-list="['by']"/>
   `
 })
 
-export const regionSpecifics = () => ({
+export const NC = () => ({
   components: { LicenseIconography },
   template: `
-    <LicenseIconography :icon-list="['nc-eu', 'nc-jp']"/>
+    <LicenseIconography :icon-list="['nc']"/>
   `
 })
 
-export const domainSpecifics = () => ({
+export const ND = () => ({
   components: { LicenseIconography },
   template: `
-    <LicenseIconography :icon-list="['sampling', 'sampling-plus', 'remix', 'share']"/>
+    <LicenseIconography :icon-list="['nd']"/>
   `
 })
 
-export const publicWorks = () => ({
+export const SA = () => ({
   components: { LicenseIconography },
   template: `
-    <LicenseIconography :icon-list="['zero', 'pd']"/>
+    <LicenseIconography :icon-list="['sa']"/>
+  `
+})
+
+export const NC_EU = () => ({
+  components: { LicenseIconography },
+  template: `
+    <LicenseIconography :icon-list="['nc-eu']"/>
+  `
+})
+
+export const NC_JP = () => ({
+  components: { LicenseIconography },
+  template: `
+    <LicenseIconography :icon-list="['nc-jp']"/>
+  `
+})
+
+export const sampling = () => ({
+  components: { LicenseIconography },
+  template: `
+    <LicenseIconography :icon-list="['sampling']"/>
+  `
+})
+
+export const samplingPlus = () => ({
+  components: { LicenseIconography },
+  template: `
+    <LicenseIconography :icon-list="['sampling-plus']"/>
+  `
+})
+
+export const remix = () => ({
+  components: { LicenseIconography },
+  template: `
+    <LicenseIconography :icon-list="['remix']"/>
+  `
+})
+
+export const share = () => ({
+  components: { LicenseIconography },
+  template: `
+    <LicenseIconography :icon-list="['share']"/>
+  `
+})
+
+export const zero = () => ({
+  components: { LicenseIconography },
+  template: `
+    <LicenseIconography :icon-list="['zero']"/>
+  `
+})
+
+export const PD = () => ({
+  components: { LicenseIconography },
+  template: `
+    <LicenseIconography :icon-list="['pd']"/>
   `
 })
