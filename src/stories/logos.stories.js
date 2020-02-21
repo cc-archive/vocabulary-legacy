57ac3788a44c2c16644b317d0da07f98490dda7b
Dhruv Bhanushali <dhruv_b@live.com>
2020-02-22 00:49:31 +0530

    Replace styles with Bulma color helpers

--- src/stories/logos.stories.js
+++ src/stories/logos.stories.js
@@ -13,40 +13,40 @@ const wordmark = '' +
   '<svg width="266.5205" height="64" viewBox="0 0 304 73" xmlns="http://www.w3.org/2000/svg">' +
   '<use href="logos/cc/wordmark.svg#creativecommons"></use>' +
   '</svg>'
-export const CCWordmark = () => wordmark
-export const CCWordmarkWhite = () => `<div style="color:white;">${wordmark}</div>`
+export const CCWordmark = () => `<div class="has-text-black">${wordmark}</div>`
+export const CCWordmarkWhite = () => `<div class="has-text-white">${wordmark}</div>`
 CCWordmarkWhite.story = invertedBackground
 
 const lettermark = '' +
   '<svg width="64" height="64" viewBox="5.5 -3.5 64 64" xmlns="http://www.w3.org/2000/svg">' +
   '<use href="logos/cc/lettermark.svg#creativecommons"></use>' +
   '</svg>'
-export const CCLettermark = () => lettermark
-export const CCLettermarkWhite = () => `<div style="color:white;">${lettermark}</div>`
+export const CCLettermark = () => `<div class="has-text-black">${lettermark}</div>`
+export const CCLettermarkWhite = () => `<div class="has-text-white">${lettermark}</div>`
 CCLettermarkWhite.story = invertedBackground
 
 const letterheart = '' +
   '<svg width="64" height="64" viewBox="0 0 46296.26 40689.13" xmlns="http://www.w3.org/2000/svg">' +
   '<use href="logos/cc/letterheart.svg#creativecommons"></use>' +
   '</svg>'
-export const CCLetterheart = () => letterheart
-export const CCLetterheartWhite = () => `<div style="color:white;">${letterheart}</div>`
+export const CCLetterheart = () => `<div class="has-text-black">${letterheart}</div>`
+export const CCLetterheartWhite = () => `<div class="has-text-white">${letterheart}</div>`
 CCLetterheartWhite.story = invertedBackground
 
 const globalNetwork = '' +
   '<svg width="250" height="79" viewBox="0 0 250 79" xmlns="http://www.w3.org/2000/svg">' +
   '<use href="logos/products/global_network.svg#globalnetwork"></use>' +
   '</svg>'
-export const CCGlobalNetwork = () => globalNetwork
-export const CCGlobalNetworkWhite = () => `<div style="color:white;">${globalNetwork}</div>`
+export const CCGlobalNetwork = () => `<div class="has-text-black">${globalNetwork}</div>`
+export const CCGlobalNetworkWhite = () => `<div class="has-text-white">${globalNetwork}</div>`
 CCGlobalNetworkWhite.story = invertedBackground
 
 const openSource = '' +
   '<svg width="250" height="76" viewBox="0 0 250 76" xmlns="http://www.w3.org/2000/svg">' +
   '<use href="logos/products/open_source.svg#opensource"></use>' +
   '</svg>'
-export const CCOpenSource = () => openSource
-export const CCOpenSourceWhite = () => `<div style="color:white;">${openSource}</div>`
+export const CCOpenSource = () => `<div class="has-text-black">${openSource}</div>`
+export const CCOpenSourceWhite = () => `<div class="has-text-white">${openSource}</div>`
 CCOpenSourceWhite.story = invertedBackground
 
 const certificates = '' +
@@ -60,22 +60,22 @@ const certificatesMonochrome = '' +
   '<use href="logos/products/certificates.svg#monochrome"></use>' +
   '</svg>'
 export const CCCertificates = () => certificates
-export const CCCertificatesBlack = () => `<div style="color: black;">${certificatesMonochrome}</div>`
-export const CCCertificatesWhite = () => `<div style="color: white;">${certificatesMonochrome}</div>`
+export const CCCertificatesBlack = () => `<div class="has-text-black">${certificatesMonochrome}</div>`
+export const CCCertificatesWhite = () => `<div class="has-text-white">${certificatesMonochrome}</div>`
 CCCertificatesWhite.story = invertedBackground
 
 const vocabulary = '' +
   '<svg width="250" height="41" viewBox="0 0 250 41" xmlns="http://www.w3.org/2000/svg">' +
   '<use href="logos/products/vocabulary.svg#vocabulary"></use>' +
   '</svg>'
-export const CCVocabulary = () => vocabulary
-export const CCVocabularyWhite = () => `<div style="color: white;">${vocabulary}</div>`
+export const CCVocabulary = () => `<div class="has-text-black">${vocabulary}</div>`
+export const CCVocabularyWhite = () => `<div class="has-text-white">${vocabulary}</div>`
 CCVocabularyWhite.story = invertedBackground
 
 const search = '' +
   '<svg width="250" height="81" viewBox="0 0 250 81" xmlns="http://www.w3.org/2000/svg">' +
   '<use href="logos/products/search.svg#search"></use>' +
   '</svg>'
-export const CCSearch = () => search
-export const CCSearchWhite = () => `<div style="color: white;">${search}</div>`
+export const CCSearch = () => `<div class="has-text-black">${search}</div>`
+export const CCSearchWhite = () => `<div class="has-text-white">${search}</div>`
 CCSearchWhite.story = invertedBackground
