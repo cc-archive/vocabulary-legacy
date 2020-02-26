7f03cd4b072f83b35124fefc9ee527b061bb4018
Chidiebere <chidex5@gmail.com>
2020-02-26 04:03:48 +0100

    Create Header stories and styles

---
+++ src/stories/header.stories.js
@@ -0,0 +1,121 @@
+import { CCLogomark } from './logos.stories'
+
+export default { title: 'Header' }
+
+export const bigHeader = () =>
+    `<header class="header big">
+        <div class="container">
+            <a href="#" class="explore-tab">Explore CC</a>
+            <nav class="navbar" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                    <a class="navbar-item" href="#" >
+                    ${CCLogomark()}
+                    </a>
+
+                    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+
+                <div id="navbarBasicExample" class="navbar-menu">
+                    <div class="navbar-end">
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a href="#" class="navbar-link" is-active>
+                            <h5>lorem </h5>
+                        </a>
+                        <div class="navbar-dropdown">
+                            <a class="navbar-item"><h5> item one </h5></a>
+                            <a class="navbar-item"><h5> item two </h5></a>
+                            <a class="navbar-item"><h5> item three </h5></a>
+                            <a class="navbar-item"><h5> item four </h5></a>
+                            <a class="navbar-item"><h5> item five </h5></a>
+                        </div>
+                    </div>
+                    <a href="#" class="navbar-item">
+                        <h5>lorem </h5>
+                    </a>
+                    <a href="#" class="navbar-item">
+                        <h5>lorem </h5>
+                    </a>
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a href="#" class="navbar-link">
+                            <h5>lorem </h5>
+                        </a>
+                        <div class="navbar-dropdown">
+                            <a class="navbar-item"><h5> item one </h5></a>
+                            <a class="navbar-item"><h5> item two </h5></a>
+                            <a class="navbar-item"><h5> item three </h5></a>
+                            <a class="navbar-item"><h5> item four </h5></a>
+                            <a class="navbar-item"><h5> item five </h5></a>
+                        </div>
+                    </div>
+                    <a href="#" class="navbar-item">
+                        <h5>lorem </h5>
+                    </a>
+            
+                </div>
+            </nav>
+        </div>
+    </header>
+`
+
+export const smallHeader = () =>
+    `<header class="header small">
+        <div class="container">
+            <a href="#" class="explore-tab">Explore CC</a>
+            <nav class="navbar" role="navigation" aria-label="main navigation">
+                <div class="navbar-brand">
+                    <a class="navbar-item" href="#" >
+                    ${CCLogomark()}
+                    </a>
+
+                    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+
+                <div id="navbarBasicExample" class="navbar-menu">
+                    <div class="navbar-end">
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a href="#" class="navbar-link" is-active>
+                            <h6>lorem </h6>
+                        </a>
+                        <div class="navbar-dropdown">
+                            <a class="navbar-item"><h6> item one </h6></a>
+                            <a class="navbar-item"><h6> item two </h6></a>
+                            <a class="navbar-item"><h6> item three </h6></a>
+                            <a class="navbar-item"><h6> item four </h6></a>
+                            <a class="navbar-item"><h6> item five </h6></a>
+                        </div>
+                    </div>
+                    <a href="#" class="navbar-item">
+                        <h6>lorem </h6>
+                    </a>
+                    <a href="#" class="navbar-item">
+                        <h6>lorem </h6>
+                    </a>
+                    <div class="navbar-item has-dropdown is-hoverable">
+                        <a href="#" class="navbar-link">
+                            <h6>lorem </h6>
+                        </a>
+                        <div class="navbar-dropdown">
+                            <a class="navbar-item"><h6> item one </h6></a>
+                            <a class="navbar-item"><h6> item two </h6></a>
+                            <a class="navbar-item"><h6> item three </h6></a>
+                            <a class="navbar-item"><h6> item four </h6></a>
+                            <a class="navbar-item"><h6> item five </h6></a>
+                        </div>
+                    </div>
+                    <a href="#" class="navbar-item">
+                        <h6>lorem </h6>
+                    </a>
+            
+                </div>
+            </nav>
+        </div>
+    </header>
+`
