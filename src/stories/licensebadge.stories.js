07e60e9147463a1e0dca54eeed3aefdc935890c9
dhruvi16 <dhruvibutti9477@gmail.com>
2020-02-26 10:35:15 +0530

    Add licensebadge to stories

---
+++ src/stories/licensebadge.stories.js
@@ -0,0 +1,7 @@
+export default { title: 'LicenseBadge' }
+
+const by = '' +
+  '<svg width="266.5205" height="64" viewBox="0 0 304 73" xmlns="http://www.w3.org/2000/svg">' +
+  '<use href="logos/licensebadge/by.svg"></use>' +
+  '</svg>'
+export const BY = () => `<div>${by}</div>`
