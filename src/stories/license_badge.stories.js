94c870ee9425cffadd073e0bc8abf151c8d8e06c
Dhruv Bhanushali <dhruv_b@live.com>
2020-02-26 16:11:43 +0530

    Fix bug in story caused by missing ID

--- src/stories/license_badge.stories.js
+++ src/stories/license_badge.stories.js
@@ -1,7 +1,7 @@
 export default { title: 'LicenseBadge' }
 
 const by = '' +
-  '<svg width="266.5205" height="64" viewBox="0 0 304 73" xmlns="http://www.w3.org/2000/svg">' +
-  '<use href="logos/licensebadge/by.svg"></use>' +
+  '<svg xmlns="http://www.w3.org/2000/svg">' +
+  '<use href="license_badges/big/by.svg#layer1"></use>' +
   '</svg>'
 export const BY = () => `<div>${by}</div>`
