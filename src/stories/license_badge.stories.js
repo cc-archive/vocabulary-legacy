28148ae3c6b3332cc34dcaebfce05f50977b52a3
dhruvi16 <dhruvibutti9477@gmail.com>
2020-02-27 11:23:46 +0530

    Add the remaining licensebadges

--- src/stories/license_badge.stories.js
+++ src/stories/license_badge.stories.js
@@ -5,3 +5,51 @@ const by = '' +
   '<use href="license_badges/big/by.svg#layer1"></use>' +
   '</svg>'
 export const BY = () => `<div>${by}</div>`
+
+const bysa = '' +
+  '<svg xmlns="http://www.w3.org/2000/svg">' +
+  '<use href="license_badges/big/by_sa.svg#layer1"></use>' +
+  '</svg>'
+export const BYSA = () => `<div>${bysa}</div>`
+
+const bynd = '' +
+  '<svg xmlns="http://www.w3.org/2000/svg">' +
+  '<use href="license_badges/big/by_nd.svg#layer1"></use>' +
+  '</svg>'
+export const BYND = () => `<div>${bynd}</div>`
+
+const bync = '' +
+  '<svg xmlns="http://www.w3.org/2000/svg">' +
+  '<use href="license_badges/big/by_nc.svg#layer1"></use>' +
+  '</svg>'
+export const BYNC = () => `<div>${bync}</div>`
+
+const bynceu = '' +
+  '<svg xmlns="http://www.w3.org/2000/svg">' +
+  '<use href="license_badges/big/by_nc.eu.svg#layer1"></use>' +
+  '</svg>'
+export const BYNCEU = () => `<div>${bynceu}</div>`
+
+const byncsa = '' +
+  '<svg xmlns="http://www.w3.org/2000/svg">' +
+  '<use href="license_badges/big/by_nc_sa.svg#layer1"></use>' +
+  '</svg>'
+export const BYNCSA = () => `<div>${byncsa}</div>`
+
+const byncnd = '' +
+  '<svg xmlns="http://www.w3.org/2000/svg">' +
+  '<use href="license_badges/big/by_nc_nd.svg#layer1"></use>' +
+  '</svg>'
+export const BYNCND = () => `<div>${byncnd}</div>`
+
+const byncsaeu = '' +
+  '<svg xmlns="http://www.w3.org/2000/svg">' +
+  '<use href="license_badges/big/by_nc_sa.eu.svg#layer1"></use>' +
+  '</svg>'
+export const BYNCSAEU = () => `<div>${byncsaeu}</div>`
+
+const byncndeu = '' +
+  '<svg xmlns="http://www.w3.org/2000/svg">' +
+  '<use href="license_badges/big/by_nc_nd.eu.svg#layer1"></use>' +
+  '</svg>'
+export const BYNCNDEU = () => `<div>${byncndeu}</div>`
