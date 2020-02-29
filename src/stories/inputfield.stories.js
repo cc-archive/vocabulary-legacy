e68500cb063ad3131bd99ba92755debb32c8591f
Dhruv Bhanushali <dhruv_b@live.com>
2020-02-29 10:44:46 +0530

    Fix linting errors

--- src/stories/inputfield.stories.js
+++ src/stories/inputfield.stories.js
@@ -1,5 +1,5 @@
 export default { title: 'InputField' }
 
-export const Normal = () => `<input class="input" type="text" placeholder="Enter text here"/>`
+export const Normal = () => '<input class="input" type="text" placeholder="Enter text here"/>'
 
-export const Big = () => `<input class="input is-large" type="text" placeholder="Enter text here"/>`
+export const Big = () => '<input class="input is-large" type="text" placeholder="Enter text here"/>'
