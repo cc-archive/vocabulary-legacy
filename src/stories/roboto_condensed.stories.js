5aa4541c33b1c31fa2459afcf96fcf90870fde73
Tanuj22 <agarwal.5@iitj.ac.in>
2020-03-01 22:35:49 +0530

    add Roboto-Condensed and SourceSansPro fonts

---
+++ src/stories/roboto_condensed.stories.js
@@ -0,0 +1,12 @@
+export default { title: 'Roboto Condensed' }
+
+export const demo = () => `
+  <style>
+  .roboto-condensed {
+    font-family: 'Roboto Condensed';
+  }
+  </style>
+  <p class="roboto-condensed">
+    The quick brown fox jumps over the lazy dog.
+  </p>
+`
---
+++ src/stories/source_sans_pro.stories.js
@@ -0,0 +1,12 @@
+export default { title: 'Source Sans Pro' }
+
+export const demo = () => `
+  <style>
+  .source-sans-pro {
+    font-family: 'Source Sans Pro';
+  }
+  </style>
+  <p class="source-sans-pro" >
+    The quick brown fox jumps over the lazy dog.
+  </p>
+`
