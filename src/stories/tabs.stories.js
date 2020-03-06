33a10bbcc13c4b7b3407c9fdb90dd98a709bcd20
kush aggarwal <kush.aggarwal110085@gmail.com>
2020-03-06 10:56:33 +0530

    tab elements updates

--- src/stories/tabs.stories.js
+++ src/stories/tabs.stories.js
@@ -2,9 +2,8 @@ export default { title: "Tabs" };
 
 export const Tabs = () => `<div class="tabs is-medium">
   <ul>
-    <li class=" is-active"><a>Pictures</a></li>
-    <li><a >Music</a></li>
-    <li><a >Videos</a></li>
-    <li><a >Documents</a></li>
+    <li class=" is-active"><a>Information</a></li>
+    <li><a >Attribution</a></li>
+    <li><a >Share</a></li>
   </ul>
 </div>`;
