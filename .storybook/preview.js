16c04269c794611980e8334f974f71ca3a00db26
Dhruv Bhanushali <dhruv_b@live.com>
2020-03-06 19:38:17 +0530

    Add MDX stories for colors and typography with documentation from Figma

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,1 +1,15 @@
+import { addDecorator, addParameters, } from '@storybook/html'
+
+import { withKnobs } from '@storybook/addon-knobs'
+
 import '../dist/css/vocabulary.css'
+
+addParameters({
+  backgrounds: [
+    { name: 'canvas', value: '#f5f5f5', default: true },
+    { name: 'white', value: '#ffffff' },
+    { name: 'black', value: '#000000' }
+  ]
+})
+
+addDecorator(withKnobs)
