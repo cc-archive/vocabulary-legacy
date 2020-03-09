cf5f2d8263d039ed6138da1a306bb9fffeb5ff6f
Breno Ferreira <bferreira@windowslive.com>
2020-03-09 15:25:53 -0300

    update button styles

--- src/stories/buttons.stories.js
+++ src/stories/buttons.stories.js
@@ -1,6 +1,12 @@
 export default { title: 'Buttons' }
 
-export const Button = () => '<button class="button big">Button</button>'
+export const BigButton = () => '<button class="button big">Button</button>'
+
+export const MediumButton = () => '<button class="button">Button</button>'
+
+export const SmallButton = () => '<button class="button small">Button</button>'
+
+export const TinyButton = () => '<button class="button tiny">Button</button>'
 
 export const ButtonPrimary = () => '<button class="button is-primary big">Button</button>'
 
@@ -13,3 +19,5 @@ export const ButtonSuccess = () => '<button class="button is-success">Button</bu
 export const ButtonWarning = () => '<button class="button is-warning">Button</button>'
 
 export const ButtonError = () => '<button class="button is-danger">Button</button>'
+
+export const Label = () => '<button class="button label">Label</button>'
