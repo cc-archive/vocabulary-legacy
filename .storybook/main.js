4fc98476d4e0de1b3fbabaf2bc8096eb335e1d68
dhruvi16 <dhruvibutti9477@gmail.com>
2020-03-11 14:38:28 +0530

    Change the syntax to MDX

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,8 @@
 module.exports = {
   stories: [
     './welcome.stories.js',
-    '../src/**/*.stories.[tj]s'
+    '../src/**/*.stories.[tj]s',
+    '../src/**/*.stories.mdx'
   ],
   addons: [
     '@storybook/addon-knobs',
