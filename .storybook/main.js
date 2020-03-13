29df0594da214eb10c949532d283878825265abb
Breno Ferreira <bferreira@windowslive.com>
2020-03-13 16:11:47 -0300

    reverts storybook config change

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,9 @@
 module.exports = {
   stories: [
     './welcome.stories.mdx',
+    '../src/stories/color.stories.mdx',
+    '../src/stories/typography.stories.mdx',
+    '../src/stories/spacing.stories.mdx',
     '../src/**/*.stories.[tj]s',
     '../src/**/*.stories.mdx'
   ],
