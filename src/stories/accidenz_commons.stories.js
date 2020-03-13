eae8171711abec23adf638ea288340ce1b44124c
kush aggarwal <kush.aggarwal110085@gmail.com>
2020-03-14 02:50:39 +0530

    issues fixed (indentation, semicolon, class names)

--- src/stories/accidenz_commons.stories.js
+++ src/stories/accidenz_commons.stories.js
@@ -1,4 +1,4 @@
-export default { title: 'Accidenz Commons' };
+export default { title: 'Accidenz Commons' }
 
 export const demo = () => `
   <style>
@@ -12,111 +12,183 @@ export const demo = () => `
 
 `
 
-export const UpperCaseLetters = () => `
-  <table>
-        <tr>
-            <th class="size">A</th>
-            <th class="size">B</th>
-            <th class="size">C</th>
-            <th class="size">D</th>
-            <th class="size">E</th>
-            <th class="size">F</th>
-            <th class="size">G</th>
-        </tr>
-    <tr>
-        <th class="size">H</th>
-        <th class="size">I</th>
-        <th class="size">J</th>
-        <th class="size">K</th>
-        <th class="size">L</th>
-        <th class="size">M</th>
-        <th class="size">N</th>
-    </tr>
-    <tr>
-        <th class="size">O</th>
-        <th class="size">P</th>
-        <th class="size">Q</th>
-        <th class="size">R</th>
-        <th class="size">S</th>
-        <th class="size">T</th>
-        <th class="size">U</th>
-    </tr>
-    <tr>
-        <th class="size">V</th>
-        <th class="size">W</th>
-        <th class="size">X</th>
-        <th class="size">Y</th>
-        <th class="size">Z</th>
-    </tr>
-  </table>
+export const UpperCaseLetters = () => {
+    return `
+        <style>
+            .size {
+            font-size: 24pt;
+            font-family: "Accidenz Commons";
 
-  <h3 class="exampleHeader">Example</h3>
-  <p class="fontEg">THE QUICK BROWN FOX JUMPS OVER LAZY DOG</p>
-`
+            border: 3px solid rgb(85, 77, 77);
+            color: rgb(85, 77, 77);
+            padding: 25px;
+            }
 
-export const LowerCaseLetters = () => `
-  <table>
-        <tr>
-            <th class="size">a</th>
-            <th class="size">b</th>
-            <th class="size">c</th>
-            <th class="size">d</th>
-            <th class="size">e</th>
-            <th class="size">f</th>
-            <th class="size">g</th>
-        </tr>
-    <tr>
-        <th class="size">h</th>
-        <th class="size">i</th>
-        <th class="size">j</th>
-        <th class="size">k</th>
-        <th class="size">l</th>
-        <th class="size">m</th>
-        <th class="size">n</th>
-    </tr>
-    <tr>
-        <th class="size">o</th>
-        <th class="size">p</th>
-        <th class="size">q</th>
-        <th class="size">r</th>
-        <th class="size">s</th>
-        <th class="size">t</th>
-        <th class="size">u</th>
-    </tr>
-    <tr>
-        <th class="size">v</th>
-        <th class="size">w</th>
-        <th class="size">x</th>
-        <th class="size">y</th>
-        <th class="size">z</th>
-    </tr>
-  </table>
+            .fontEg {
+            font-family: "Accidenz Commons";
+            font-size: 20pt;
+            }
 
-  
-<h3 class="exampleHeader">Example</h3>
-  <p class="fontEg">the quick brown fox jumps over the lazy dog </p>
-`
+            .example-header {
+            margin-top: 20px;
+            }
+            th:hover {
+            background-color: rgb(224, 214, 214);
+            }
+        </style>
+        <table>
+                <tr>
+                    <th class="size">A</th>
+                    <th class="size">B</th>
+                    <th class="size">C</th>
+                    <th class="size">D</th>
+                    <th class="size">E</th>
+                    <th class="size">F</th>
+                    <th class="size">G</th>
+                </tr>
+            <tr>
+                <th class="size">H</th>
+                <th class="size">I</th>
+                <th class="size">J</th>
+                <th class="size">K</th>
+                <th class="size">L</th>
+                <th class="size">M</th>
+                <th class="size">N</th>
+            </tr>
+            <tr>
+                <th class="size">O</th>
+                <th class="size">P</th>
+                <th class="size">Q</th>
+                <th class="size">R</th>
+                <th class="size">S</th>
+                <th class="size">T</th>
+                <th class="size">U</th>
+            </tr>
+            <tr>
+                <th class="size">V</th>
+                <th class="size">W</th>
+                <th class="size">X</th>
+                <th class="size">Y</th>
+                <th class="size">Z</th>
+            </tr>
+        </table>
 
-export const Numbers = () => `
-  <table class="table">
-    <tr>
-        <th class="size">0</th>
-        <th class="size">1</th>
-        <th class="size">2</th>
-        <th class="size">3</th>
-        <th class="size">4</th>
+        <h3 class="example-header">Example</h3>
+        <p class="fontEg">THE QUICK BROWN FOX JUMPS OVER LAZY DOG</p>
+    `
+}
+
+export const LowerCaseLetters = () => {
+    return `
+        <style>
+            .size {
+            font-size: 24pt;
+            font-family: "Accidenz Commons";
+
+            border: 3px solid rgb(85, 77, 77);
+            color: rgb(85, 77, 77);
+            padding: 25px;
+            }
+
+            .fontEg {
+            font-family: "Accidenz Commons";
+            font-size: 20pt;
+            }
+
+            .example-header {
+            margin-top: 20px;
+            }
+            th:hover {
+            background-color: rgb(224, 214, 214);
+            }
+        </style>
+        <table>
+                <tr>
+                    <th class="size">a</th>
+                    <th class="size">b</th>
+                    <th class="size">c</th>
+                    <th class="size">d</th>
+                    <th class="size">e</th>
+                    <th class="size">f</th>
+                    <th class="size">g</th>
+                </tr>
+            <tr>
+                <th class="size">h</th>
+                <th class="size">i</th>
+                <th class="size">j</th>
+                <th class="size">k</th>
+                <th class="size">l</th>
+                <th class="size">m</th>
+                <th class="size">n</th>
+            </tr>
+            <tr>
+                <th class="size">o</th>
+                <th class="size">p</th>
+                <th class="size">q</th>
+                <th class="size">r</th>
+                <th class="size">s</th>
+                <th class="size">t</th>
+                <th class="size">u</th>
+            </tr>
+            <tr>
+                <th class="size">v</th>
+                <th class="size">w</th>
+                <th class="size">x</th>
+                <th class="size">y</th>
+                <th class="size">z</th>
+            </tr>
+        </table>
+
         
-    </tr>
-    <tr>
-        <th class="size">5</th>
-        <th class="size">6</th>
-        <th class="size">7</th>
-        <th class="size">8</th>
-        <th class="size">9</th>
-    </tr>
-    
-  </table>
+        <h3 class="example-header">Example</h3>
+        <p class="fontEg">the quick brown fox jumps over the lazy dog </p>
+    `
+}
 
-  <h3 class="exampleHeader">Example</h3>
-  <h4 class="fontEg">0123456789</h4>
-`
+export const Numbers = () => {
+    return `
+        <style>
+            .size {
+            font-size: 24pt;
+            font-family: "Accidenz Commons";
+
+            border: 3px solid rgb(85, 77, 77);
+            color: rgb(85, 77, 77);
+            padding: 25px;
+            }
+
+            .fontEg {
+            font-family: "Accidenz Commons";
+            font-size: 20pt;
+            }
+
+            .example-header {
+            margin-top: 20px;
+            }
+            th:hover {
+            background-color: rgb(224, 214, 214);
+            }
+        </style>
+        <table class="table">
+            <tr>
+                <th class="size">0</th>
+                <th class="size">1</th>
+                <th class="size">2</th>
+                <th class="size">3</th>
+                <th class="size">4</th>
+                
+            </tr>
+            <tr>
+                <th class="size">5</th>
+                <th class="size">6</th>
+                <th class="size">7</th>
+                <th class="size">8</th>
+                <th class="size">9</th>
+            </tr>
+            
+        </table>
+
+        <h3 class="example-header">Example</h3>
+        <h4 class="fontEg">0123456789</h4>
+    `
+}
