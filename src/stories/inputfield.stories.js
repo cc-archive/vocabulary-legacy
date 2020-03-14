680b68051583c90ea50c4e5adfb5b42bdd5c213e
Dhruv Bhanushali <dhruv_b@live.com>
2020-03-15 00:38:14 +0530

    Update documentation for form components and the locale switcher

--- src/stories/inputfield.stories.js
+++
@@ -1,5 +0,0 @@
-export default { title: 'InputField' }
-
-export const Normal = () => '<input class="input" type="text" placeholder="Enter text here"/>'
-
-export const Big = () => '<input class="input is-large" type="text" placeholder="Enter text here"/>'
