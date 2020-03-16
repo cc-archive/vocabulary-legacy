8fea82a0b5b7cf191ae395e0ed7b41e3843ce93d
kush aggarwal <kush.aggarwal110085@gmail.com>
2020-03-16 17:40:58 +0530

    issues resolved - indentaion additional-files code-optimization

---
+++ src/stories/helper.js
@@ -0,0 +1,41 @@
+export const tablecomponents =
+    `
+        <table>
+          <tr>
+            <th class="size">A</th>
+            <th class="size">B</th>
+            <th class="size">C</th>
+            <th class="size">D</th>
+            <th class="size">E</th>
+            <th class="size">F</th>
+            <th class="size">G</th>
+         </tr>
+         <tr>
+            <th class="size">H</th>
+            <th class="size">I</th>
+            <th class="size">J</th>
+            <th class="size">K</th>
+            <th class="size">L</th>
+            <th class="size">M</th>
+            <th class="size">N</th>
+         </tr>
+         <tr>
+            <th class="size">O</th>
+            <th class="size">P</th>
+            <th class="size">Q</th>
+            <th class="size">R</th>
+            <th class="size">S</th>
+            <th class="size">T</th>
+            <th class="size">U</th>
+         </tr>
+         <tr>
+            <th class="size">V</th>
+            <th class="size">W</th>
+            <th class="size">X</th>
+            <th class="size">Y</th>
+            <th class="size">Z</th>
+         </tr>
+        </table>
+
+        
+    `
