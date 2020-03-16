ef025f303574970fb7b668a4471baa5df5f01eae
Dhruv Bhanushali <dhruv_b@live.com>
2020-03-16 13:02:55 +0530

    Add a dummy unit test to start with

--- tests/sass.test.js
+++ tests/sass.test.js
@@ -1,5 +1,5 @@
-var path = require('path');
-var sassTrue = require('sass-true');
+const path = require('path')
+const sassTrue = require('sass-true')
 
-var sassFile = path.join(__dirname, 'tests.scss');
+const sassFile = path.join(__dirname, 'tests.scss')
 sassTrue.runSass({ file: sassFile }, { describe, it });
