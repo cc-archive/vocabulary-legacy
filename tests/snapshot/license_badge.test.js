9b9b7898717d799996774e02d09424143b1d877a
Your Name <you@example.com>
2020-03-30 02:36:35 +0530

    License_Tests

---
+++ tests/snapshot/license_badge.test.js
@@ -0,0 +1,67 @@
+import { setViewport, goTo } from './helpers'
+
+describe('License Badge', () => {
+  it('BY', async () => {
+    await setViewport(page)
+    await goTo(page, 'assets-License-Badge-BY')
+    const image = await page.screenshot()
+
+    expect(image).toMatchImageSnapshot()
+  })
+
+  it('BY SA', async () => {
+    await setViewport(page)
+    await goTo(page, 'assets-License-Badge-BY-SA')
+    const image = await page.screenshot()
+
+    expect(image).toMatchImageSnapshot()
+  })
+
+  it('BY ND', async () => {
+    await setViewport(page)
+    await goTo(page, 'assets-License-Badge-BY-ND')
+    const image = await page.screenshot()
+
+    expect(image).toMatchImageSnapshot()
+  })
+
+  it('BY NC', async () => {
+    await setViewport(page)
+    await goTo(page, 'assets-License-Badge-BY-NC')
+    const image = await page.screenshot()
+
+    expect(image).toMatchImageSnapshot()
+  })
+
+  it('BY NC SA', async () => {
+    await setViewport(page)
+    await goTo(page, 'assets-License-Badge-BY-NC-SA')
+    const image = await page.screenshot()
+
+    expect(image).toMatchImageSnapshot()
+  })
+
+  it('BY NC ND', async () => {
+    await setViewport(page)
+    await goTo(page, 'assets-License-Badge-BY-NC-ND')
+    const image = await page.screenshot()
+
+    expect(image).toMatchImageSnapshot()
+  })
+
+  it('CC ZERO', async () => {
+    await setViewport(page)
+    await goTo(page, 'assets-License-Badge-CC-Zero')
+    const image = await page.screenshot()
+
+    expect(image).toMatchImageSnapshot()
+  })
+
+  it('PUBLIC DOMAIN', async () => {
+    await setViewport(page)
+    await goTo(page, 'assets-License-Badge-Public-Domain')
+    const image = await page.screenshot()
+
+    expect(image).toMatchImageSnapshot()
+  })
+})
