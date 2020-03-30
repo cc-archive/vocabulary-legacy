1a180c913f4cc5a0f9bebe2cdaffd80272ccd176
Your Name <you@example.com>
2020-03-30 18:38:26 +0530

    Visual Test for Tables

---
+++ tests/snapshot/table.test.js
@@ -0,0 +1,21 @@
+import { setViewport, goTo } from './helpers'
+
+describe('Table', () => {
+  it('BasicContent', async () => {
+    await setViewport(page)
+    await goTo(page, 'layouts-table--basic-content')
+    const image = await page.screenshot()
+
+    expect(image).toMatchImageSnapshot()
+  })
+
+  it('RichContent', async () => {
+    await setViewport(page)
+    await goTo(page, 'layouts-table--rich-content')
+    const image = await page.screenshot()
+
+    expect(image).toMatchImageSnapshot()
+  })
+
+  
+})
