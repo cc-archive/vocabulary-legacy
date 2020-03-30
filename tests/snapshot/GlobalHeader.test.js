509d5325f2eb37bcfc087f8334cca94d3cb22575
Your Name <you@example.com>
2020-03-30 17:46:37 +0530

    Global Header Tests

---
+++ tests/snapshot/GlobalHeader.test.js
@@ -0,0 +1,19 @@
+import { setViewport, goTo } from './helpers'
+
+describe('GlobalHeader', () => {
+  it('Inactive', async () => {
+    await setViewport(page)
+    await goTo(page, 'patterns-global-header--inactive')
+    const image = await page.screenshot()
+
+    expect(image).toMatchImageSnapshot()
+  })
+
+  it('Active', async () => {
+    await setViewport(page)
+    await goTo(page, 'patterns-global-header--active')
+    const image = await page.screenshot()
+
+    expect(image).toMatchImageSnapshot()
+  })
+})
