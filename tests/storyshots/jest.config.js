1debb561227685a55734cb5dfb86146761f4c352
Tanuj22 <agarwal.5@iitj.ac.in>
2020-03-31 02:54:34 +0530

    add jest pupeteer

--- tests/storyshots/jest.config.js
+++ tests/storyshots/jest.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   testRegex: './*\\.test\\.js$',
   transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-    '^.+\\.mdx?$': '@storybook/addon-docs/jest-transform-mdx'
-  },
-  setupFilesAfterEnv: [
-    './storyshots.test.js'
-  ]
+    '^.+\\.jsx?$': 'babel-jest'
+  }
 }
--- tests/storyshots/storyshots.test.js
+++ tests/storyshots/storyshots.test.js
@@ -1,3 +1,9 @@
-import initStoryshots from '@storybook/addon-storyshots'
+import path from 'path';
+import initStoryshots, { multiSnapshotWithOptions } from '@storybook/addon-storyshots';
 
-initStoryshots({framework: 'html'})
+initStoryshots({
+  framework: 'html',
+  integrityOptions: { cwd: path.resolve(__dirname, '../../src/stories') },
+  configPath: path.resolve(__dirname, '../../.storybook'),
+  test: multiSnapshotWithOptions()
+});
