be65a24e1bdd7f2bf83a236fb354900ca09f2e8f
Your Name <you@example.com>
2020-03-30 20:38:28 +0530

    added Test for select

--- tests/snapshot/select.test.js
+++ tests/snapshot/select.test.js
@@ -1,9 +1,9 @@
 import { setViewport, goTo } from './helpers'
 
-describe('Locale', () => {
+describe('Select', () => {
   it('Default', async () => {
     await setViewport(page)
-    await goTo(page, 'patterns-locale--default-story')
+    await goTo(page, 'form-select--default-story')
     const image = await page.screenshot()
 
     expect(image).toMatchImageSnapshot()
