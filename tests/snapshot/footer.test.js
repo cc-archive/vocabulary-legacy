b70f05e898814a0fcda987f5db5e61deeef80180
Your Name <you@example.com>
2020-03-30 20:14:34 +0530

    Updated Story Name

--- tests/snapshot/footer.test.js
+++ tests/snapshot/footer.test.js
@@ -3,7 +3,7 @@ import { setViewport, goTo } from './helpers'
 describe('Footer', () => {
   it('Default', async () => {
     await setViewport(page)
-    await goTo(page, 'patterns-footer--default')
+    await goTo(page, 'patterns-footer--default-story')
     const image = await page.screenshot()
 
     expect(image).toMatchImageSnapshot()
