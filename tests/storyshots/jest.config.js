c93cd8e6a91931dbf69f6f43978796cf1ceefed0
Tanuj22 <agarwal.5@iitj.ac.in>
2020-04-01 17:48:06 +0530

    fix error with import statements and added styleMocks

--- tests/storyshots/jest.config.js
+++ tests/storyshots/jest.config.js
@@ -1,6 +1,17 @@
 module.exports = {
   testRegex: './*\\.test\\.js$',
+  moduleNameMapper: {
+    '\\.(css|scss|stylesheet)$': '<rootDir>/__mocks__/styleMock.js'
+  },
   transform: {
-    '^.+\\.jsx?$': 'babel-jest'
-  }
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.mdx?$': '@storybook/addon-docs/jest-transform-mdx'
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(storybook-addon-designs)/)'
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/dist/'
+  ]
 }
