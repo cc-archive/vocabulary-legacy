c9bc3e485f4f5a760c5911f5c445db98292074a5
Tanuj22 <agarwal.5@iitj.ac.in>
2020-04-01 23:24:29 +0530

    fix error with jsx and added spanshots

--- tests/storyshots/jest.config.js
+++ tests/storyshots/jest.config.js
@@ -4,14 +4,11 @@ module.exports = {
     '\\.(css|scss|stylesheet)$': '<rootDir>/__mocks__/styleMock.js'
   },
   transform: {
+    '^.+\\.stories\\.js$': '@storybook/addon-storyshots/injectFileName',
     '^.+\\.jsx?$': 'babel-jest',
     '^.+\\.mdx?$': '@storybook/addon-docs/jest-transform-mdx'
   },
   transformIgnorePatterns: [
     'node_modules/(?!(storybook-addon-designs)/)'
-  ],
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '/dist/'
   ]
 }
