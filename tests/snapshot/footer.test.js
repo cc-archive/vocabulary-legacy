5004af32af0b90d06493a6a9aa0d47d9d0150d36
Dhruv Bhanushali <dhruv_b@live.com>
2020-04-02 00:29:50 +0530

    Fix footer visual tests

--- tests/snapshot/footer.test.js
+++ tests/snapshot/footer.test.js
@@ -1,9 +1,10 @@
-import { setViewport, goTo } from './helpers'
+import { setViewport, goTo, pause } from './helpers'
 
 describe('Footer', () => {
   it('Default', async () => {
     await setViewport(page)
     await goTo(page, 'patterns-footer--default-story')
+    await pause(1000)
     const image = await page.screenshot()
 
     expect(image).toMatchImageSnapshot()
