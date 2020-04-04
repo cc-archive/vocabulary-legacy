cd5a41ff2a65dcf32220e05c927ba2246ae18a26
Dhruv Bhanushali <dhruv_b@live.com>
2020-04-04 18:14:57 +0530

    Update Jest puppeteer config making `setViewport` redundant

--- tests/snapshot/logos.test.js
+++ tests/snapshot/logos.test.js
@@ -1,8 +1,7 @@
-import { setViewport, goTo, pause } from './helpers'
+import { goTo, pause } from './helpers'
 
 describe('Logos', () => {
   it('CC Logomark', async () => {
-    await setViewport(page)
     await goTo(page, 'assets-logos--cc-logomark')
     await pause(1000)
     const image = await page.screenshot()
@@ -11,7 +10,6 @@ describe('Logos', () => {
   })
 
   it('CC Lettermark', async () => {
-    await setViewport(page)
     await goTo(page, 'assets-logos--cc-lettermark')
     await pause(1000)
     const image = await page.screenshot()
@@ -20,7 +18,6 @@ describe('Logos', () => {
   })
 
   it('CC Letterheart', async () => {
-    await setViewport(page)
     await goTo(page, 'assets-logos--cc-letterheart')
     await pause(1000)
     const image = await page.screenshot()
@@ -29,7 +26,6 @@ describe('Logos', () => {
   })
 
   it('CC Global Network', async () => {
-    await setViewport(page)
     await goTo(page, 'assets-logos--cc-global-network')
     await pause(1000)
     const image = await page.screenshot()
@@ -38,7 +34,6 @@ describe('Logos', () => {
   })
 
   it('CC Open Source', async () => {
-    await setViewport(page)
     await goTo(page, 'assets-logos--cc-open-source')
     await pause(1000)
     const image = await page.screenshot()
@@ -47,7 +42,6 @@ describe('Logos', () => {
   })
 
   it('CC Vocabulary', async () => {
-    await setViewport(page)
     await goTo(page, 'assets-logos--cc-vocabulary')
     await pause(1000)
     const image = await page.screenshot()
@@ -56,7 +50,6 @@ describe('Logos', () => {
   })
 
   it('CC Search', async () => {
-    await setViewport(page)
     await goTo(page, 'assets-logos--cc-search')
     await pause(1000)
     const image = await page.screenshot()
@@ -65,7 +58,6 @@ describe('Logos', () => {
   })
 
   it('CC Certificates Colorful', async () => {
-    await setViewport(page)
     await goTo(page, 'assets-logos--cc-certificates-colorful')
     await pause(1000)
     const image = await page.screenshot()
@@ -74,7 +66,6 @@ describe('Logos', () => {
   })
 
   it('CC Certificates Monochrome', async () => {
-    await setViewport(page)
     await goTo(page, 'assets-logos--cc-certificates-monochrome')
     await pause(1000)
     const image = await page.screenshot()
--- tests/snapshot/select.test.js
+++ tests/snapshot/select.test.js
@@ -1,8 +1,7 @@
-import { setViewport, goTo } from './helpers'
+import { goTo } from './helpers'
 
 describe('Select', () => {
   it('Default', async () => {
-    await setViewport(page)
     await goTo(page, 'form-select--default-story')
     const image = await page.screenshot()
 
