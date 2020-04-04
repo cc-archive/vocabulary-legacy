fecb16b6061d5c2d361f6dd0cfbaba9887f685bf
Dhruv Bhanushali <dhruv_b@live.com>
2020-04-04 19:45:34 +0530

    Fix broken tests

--- tests/snapshot/tab.test.js
+++ tests/snapshot/tab.test.js
@@ -1,8 +1,7 @@
-import { setViewport, goTo } from './helpers'
+import { goTo } from './helpers'
 
 describe('Tab', () => {
   it('Tab', async () => {
-    await setViewport(page)
     await goTo(page, 'layouts-tabs--tab')
     const image = await page.screenshot()
 
