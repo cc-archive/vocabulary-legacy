d2293536e31321a8667c31fc5f83e4b644248dd6
Dhruv Bhanushali <dhruv_b@live.com>
2020-04-05 09:12:23 +0530

    Setup snapshot testing and write pilot test

--- tests/snapshot/helpers.js
+++ tests/snapshot/helpers.js
@@ -1,7 +1,3 @@
-export const setViewport = async (page) => {
-  await page.setViewport({ width: 1920, height: 1080, deviceScaleFactor: 2 })
-}
-
 export const goTo = async (page, id) => {
   let storybookOrigin = process.env.STORYBOOK_ORIGIN
   if (storybookOrigin === undefined) {
