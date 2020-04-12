42475e391a4480038945ee9ecda82d7f2111bad0
Dhruv Bhanushali <dhruv_b@live.com>
2020-04-12 11:57:37 +0530

    Align directory structure with creativecommons/vocabulary

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -o errexit
+set -o errtrace
+set -o nounset
+
+trap '_es=${?};
+    _lo=${LINENO};
+    _co=${BASH_COMMAND};
+    echo "${0}: line ${_lo}: \"${_co}\" exited with a status of ${_es}";
+    exit ${_es}' ERR
+
+npm run build
+rm dist/demo.html
+cp package.json dist/
+cp README.md dist/
+cp LICENSE dist/
+
+cd dist
+npm publish --access public
