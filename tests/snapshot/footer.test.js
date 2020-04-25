a3b8a1918fc0d7778a658b5567659f2bc730f4b7
megha070 <varshney.megha070@gmail.com>
2020-04-25 18:43:39 +0530

    reduce screenshot area in footer snapshots

--- tests/snapshot/footer.test.js
+++ tests/snapshot/footer.test.js
@@ -4,8 +4,8 @@ describe('Footer', () => {
   it('Default', async () => {
     await goTo(page, 'patterns-footer--default-story')
     await pause(1000)
-    const image = await page.screenshot()
-
-    expect(image).toMatchImageSnapshot()
+    const image = await page.$('footer.main-footer')
+    const ss = await image.screenshot()
+    expect(ss).toMatchImageSnapshot()
   })
 })
