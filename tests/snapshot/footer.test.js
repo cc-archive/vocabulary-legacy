33a77885316f9365b071e323e85105c411a13e39
Megha Varshney <40516822+megha070@users.noreply.github.com>
2020-04-26 11:31:40 +0530

    changes in selector

--- tests/snapshot/footer.test.js
+++ tests/snapshot/footer.test.js
@@ -4,7 +4,7 @@ describe('Footer', () => {
   it('Default', async () => {
     await goTo(page, 'patterns-footer--default-story')
     await pause(1000)
-    const image = await page.$('footer.main-footer')
+    const image = await page.$('footer')
     const ss = await image.screenshot()
     expect(ss).toMatchImageSnapshot()
   })
