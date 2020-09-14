790e7ccf53f99da7d174ce909076716e0a958c3a
Zack Krida <zackkrida@pm.me>
2020-09-14 12:06:59 -1000

    Fix small issues in button stories

--- packages/vue-vocabulary/src/elements/Button/Button.stories.js
+++ packages/vue-vocabulary/src/elements/Button/Button.stories.js
@@ -3,17 +3,17 @@ import { sizes, theme } from '@/utils/values'
 import { addDescription } from '@/utils/addDescription'
 
 export default {
-  title: "Elements/Button",
+  title: 'Elements/Button',
   component: Button,
   argTypes: {
     size: {
-      control: { type: "select", options: sizes },
+      control: { type: 'select', options: sizes }
     },
     theme: {
-      control: { type: "select", options: theme },
-    },
-  },
-};
+      control: { type: 'select', options: theme }
+    }
+  }
+}
 
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
@@ -22,14 +22,13 @@ const Template = (args, { argTypes }) => ({
 })
 
 export const Default = Template.bind({})
-Default.args = { theme: 'primary' }
 
 export const Theme = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: { Button },
   template: `<div style="display: flex; gap: 4px;">
     <Button v-bind="$props" theme="primary">Primary</Button>
-    <Button v-bind="$props" theme="secondary">Secondary</Button>
+    <Button v-bind="$props" theme="success">Success</Button>
     <Button v-bind="$props" theme="info">Info</Button>
     <Button v-bind="$props" theme="warning">Warning</Button>
     <Button v-bind="$props" theme="danger">Danger</Button>
