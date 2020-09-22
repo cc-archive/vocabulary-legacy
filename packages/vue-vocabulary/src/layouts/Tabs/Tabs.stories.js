5d6bf5d21eec1d27d10687428a026676b9cb725a
Zack Krida <zackkrida@pm.me>
2020-09-22 11:58:33 -1000

    Fix linting by downgrading @vue/eslint-config-standard to 4.x version

--- packages/vue-vocabulary/src/layouts/Tabs/Tabs.stories.js
+++ packages/vue-vocabulary/src/layouts/Tabs/Tabs.stories.js
@@ -1,13 +1,13 @@
-import Tabs from "@/layouts/Tabs/Tabs";
-import Tab from "@/layouts/Tabs/Tab";
-import { figmaConfig } from "@/utils/helpers";
+import Tabs from '@/layouts/Tabs/Tabs'
+import Tab from '@/layouts/Tabs/Tab'
+import { figmaConfig } from '@/utils/helpers'
 import { addDescription } from '@/utils/addDescription'
 
 export default {
-  title: "Layouts/Tabs",
-  parameters: { design: figmaConfig("933%3A1"), docs: { description: { component: 'The Tabs component is a navigation element to switch between content sections, and should be used as the main content switch in a page. Tabs are added with the `<Tab>` component.' } } },
-  component: Tabs,
-};
+  title: 'Layouts/Tabs',
+  parameters: { design: figmaConfig('933%3A1'), docs: { description: { component: 'The Tabs component is a navigation element to switch between content sections, and should be used as the main content switch in a page. Tabs are added with the `<Tab>` component.' } } },
+  component: Tabs
+}
 
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
@@ -17,12 +17,12 @@ const Template = (args, { argTypes }) => ({
     <Tab title="Pictures">Pictures content.</Tab>
     <Tab title="Videos">Videos content.</Tab>
     <Tab title="Document">Document content.</Tab>
-  </Tabs>`,
-});
+  </Tabs>`
+})
 
-export const Default = Template.bind({});
-Default.args = {};
+export const Default = Template.bind({})
+Default.args = {}
 
-export const Boxed = Template.bind({});
+export const Boxed = Template.bind({})
 addDescription(Boxed, 'Set the `boxed` prop to `true` use a different variation.')
-Boxed.args = { boxed: true };
+Boxed.args = { boxed: true }
