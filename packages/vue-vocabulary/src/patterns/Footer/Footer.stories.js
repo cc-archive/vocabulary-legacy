60e1d67266ee4bc53ed1c7b9117fe01241f153d2
akmadian <akmadian@gmail.com>
2020-10-05 22:22:53 -0700

    Add Footer story, start adding Footer component

---
+++ packages/vue-vocabulary/src/patterns/Footer/Footer.stories.js
@@ -0,0 +1,39 @@
+import Footer from './Footer'
+import { sizes, theme } from '@/utils/values'
+import { addDescription } from '@/utils/addDescription'
+
+export default {
+  title: 'Patterns/Footer',
+  component: Footer
+}
+
+export const items = [
+  {
+    href: 'test1.com',
+    content: 'test1'
+  },
+  {
+    href: 'test1.com',
+    content: 'test2'
+  },
+  {
+    href: 'test1.com',
+    content: 'test3'
+  },
+  {
+    href: 'test1.com',
+    content: 'test4'
+  }
+]
+
+const Template = '<Footer v-bind="$props"></Footer>'
+
+export const Default = () => ({
+  components: { Footer },
+  template: Template,
+  props: {
+    customItems: {
+      default: () => items
+    }
+  }
+})
