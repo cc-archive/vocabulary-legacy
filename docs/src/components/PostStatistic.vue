36c096d3f2cb64cf2a11296c620cbf4e065f99c3
nimishbongale <nimishnb98@gmail.com>
2020-10-11 18:43:48 +0530

    Add PostStatistic Components

---
+++ docs/src/components/PostStatistic.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="post-card">
+      <article class="card entry-post entry-statistic horizontal">
+        <header class="card-header">
+          <div class="card-statistic">
+            <span class="number">{{ number }}</span>
+            <span class="caption">{{ caption }}</span>
+          </div>
+        </header>
+        <div class="card-content">
+          <div class="content">
+            {{ description }}
+          </div>
+          <a :href="href" class="read-more">Visit Github</a>
+        </div>
+      </article>
+    </div>
+</template>
+
+<style scoped>
+ #post-card {
+     margin: 10px;
+     width: 30%;
+ }
+</style>
+
+<script>
+import '@creativecommons/vocabulary/css/vocabulary.css'
+
+export default {
+    name:'PostStatistic',
+    props:['number','caption','description','href']
+}
+</script>
---
+++ docs/src/components/PostStatisticGroup.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="post-statistic-group">
+      <PostStatistic :number="statisticList.stargazers_count" caption="Stars" description="Star the repository!" href="https://github.com/creativecommons/vocabulary"/>
+      <PostStatistic :number="statisticList.forks_count" caption="Forks" description="Want to contribute? Fork the repository!" href="https://github.com/creativecommons/vocabulary"/>
+      <PostStatistic :number="statisticList.watchers_count" caption="Watchers" description="Want to stay updated with all our activity?" href="https://github.com/creativecommons/vocabulary"/>
+    </div>
+</template>
+
+<style scoped>
+    #post-statistic-group {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        padding-top: 25px;
+        padding-bottom: 25px;
+
+        justify-content: space-evenly;
+    }
+</style>
+
+<script>
+import '@creativecommons/vocabulary/css/vocabulary.css'
+import PostStatistic from './PostStatistic'
+
+export default {
+    name:'PostStatisticGroup',
+    components: {
+        PostStatistic
+    },
+    data(){
+        return {
+            statisticList: null
+        }
+    },
+    created(){
+        fetch('https://api.github.com/repos/creativecommons/vocabulary')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            this.statisticList=data
+        });
+    }
+}
+</script>
