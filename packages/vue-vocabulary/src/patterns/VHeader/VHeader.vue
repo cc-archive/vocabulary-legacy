187e050147d9671c2fa1749a600d31df2273ce07
Olga Bulat <obulat@gmail.com>
2020-10-15 17:21:10 +0300

    Add logo, dynamic MenuItem and vocabulary styles

--- packages/vue-vocabulary/src/patterns/VHeader/VHeader.vue
+++ packages/vue-vocabulary/src/patterns/VHeader/VHeader.vue
@@ -1,6 +1,36 @@
 <template>
   <header class="vocab header">
-    <!-- TODO -->
+    <nav class="navbar" :aria-label="$t('header.aria.primary')">
+      <div class="navbar-brand has-color-white">
+        <slot name="logo">
+          <a class="logo" href="/">
+            <CCSearchLogo />
+          </a>
+        </slot>
+        <a role="button"
+           :class="{ ['navbar-burger']: true, ['is-active']: isBurgerMenuActive }"
+           :aria-label="$t('header.aria.menu')"
+           aria-expanded="false"
+           @click="toggleBurgerActive"
+           @keyup.enter="toggleBurgerActive">
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+            <span aria-hidden="true" />
+        </a>
+      </div>
+      <div :class="{ ['navbar-menu']: true, ['is-active']: isBurgerMenuActive }">
+        <div class="navbar-end">
+          <slot name="menu-items">
+            <MenuItem tag="a" href="/whatever" label="Item One" />
+            <MenuDropdown label="Item Three">
+              <MenuItem href="/whatever" label="Item Three A" />
+              <MenuItem tag = "a" href="/whatever" label="Item Three B" />
+            </MenuDropdown>
+            <MenuItem href="/whatever" label="Item Two" />
+          </slot>
+        </div>
+      </div>
+    </nav>
   </header>
 </template>
 
@@ -11,7 +41,34 @@
    * The header displays information about the site such as its branding and
    * name as well as navigation links.
    */
+  import MenuItem from './MenuItem'
+  import MenuDropdown from './MenuDropdown'
+  import CCSearchLogo from '@creativecommons/vocabulary/assets/logos/products/search.svg'
+
   export default {
-    name: 'VHeader'
+    name: 'VHeader',
+    components: {
+      MenuItem,
+      MenuDropdown,
+      CCSearchLogo
+    },
+    data: () => ({ isBurgerMenuActive: false }),
+    methods: {
+      toggleBurgerActive () {
+        this.isBurgerMenuActive = !this.isBurgerMenuActive
+      }
+    }
   }
 </script>
+<style lang="scss" scoped>
+@import '~@creativecommons/vocabulary/scss/vocabulary.scss';
+
+.logo {
+  color: black;
+  svg {
+    height: 100%;
+    width: auto;
+  }
+}
+
+</style>
