3bfd0f51c0d230c930bcb02bb5d66126ab926e1f
Nimish Bongale <nimishnb98@gmail.com>
2020-10-27 14:09:20 +0530

    Changed to vocabulary/mainjs newline revokes

--- packages/vue-vocabulary/.storybook/main.js
+++ packages/vue-vocabulary/.storybook/main.js
@@ -6,10 +6,7 @@ module.exports = {
   addons: [
     '@storybook/addon-essentials',
     '@storybook/addon-docs',
-
+    '@storybook/addon-a11y',
     'storybook-dark-mode/register'
-
-    '@storybook/addon-a11y'
-
   ]
 }
