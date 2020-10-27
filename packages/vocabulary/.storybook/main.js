2fe2d57e9ea60ae16cdb015350ca72e5fa10f299
Neeraj Anand <56186425+neeraj-2@users.noreply.github.com>
2020-10-27 11:14:55 +0530

    format update

--- packages/vocabulary/.storybook/main.js
+++ packages/vocabulary/.storybook/main.js
@@ -14,10 +14,8 @@ module.exports = {
     '@storybook/addon-backgrounds',
     '@storybook/addon-viewport',
     '@storybook/addon-docs',
-
-    'storybook-dark-mode/register'
-
-    '@storybook/addon-a11y',
+    'storybook-dark-mode/register',
+    '@storybook/addon-a11y'
 
   ]
 }
