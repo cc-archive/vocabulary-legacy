2f234a3d48f66b9f3233e0d3ece2b3e56f407795
Olga Bulat <obulat@gmail.com>
2020-11-05 16:07:53 +0300

    Small fixes for Vue buttons and update to core vocabulary

--- packages/vue-vocabulary/src/utils/values.js
+++ packages/vue-vocabulary/src/utils/values.js
@@ -2,5 +2,5 @@
  * This file is just a temporary measure until we figure out a better way
  * to structure tokens for vocabulary, and things are more consistent across components.
  */
-export const sizes = ['tiny', 'small', 'medium', 'big']
+export const sizes = ['tiny', 'small', 'medium', 'normal', 'big']
 export const theme = ['primary', 'success', 'info', 'text']
