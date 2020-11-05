4212af9d76969e26b3705f31f8412d337beb9b5a
Olga Bulat <obulat@gmail.com>
2020-11-05 16:01:45 +0300

    Add correct source code to Vue-vocabulary button stories

--- packages/vue-vocabulary/src/elements/Button/Button.stories.js
+++ packages/vue-vocabulary/src/elements/Button/Button.stories.js
@@ -22,6 +22,13 @@ const Template = (args, { argTypes }) => ({
 })
 
 export const Default = Template.bind({})
+Default.parameters = {
+  docs: {
+    source: {
+      code: `<Button>Text</Button>`
+    }
+  }
+}
 
 export const Theme = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
@@ -30,17 +37,29 @@ export const Theme = (args, { argTypes }) => ({
     <Button v-bind="$props" theme="primary">Primary</Button>
     <Button v-bind="$props" theme="success">Success</Button>
     <Button v-bind="$props" theme="info">Info</Button>
-    <Button v-bind="$props" theme="warning">Warning</Button>
-    <Button v-bind="$props" theme="danger">Danger</Button>
   </div>`
 })
 addDescription(Theme, 'Buttons can be styled with various colors. The primary button should be used for major site actions. There should rarely be more than one primary button per page.')
 Theme.args = {}
-
+Theme.parameters = {
+  docs: {
+    source: {
+      code: `<Button theme="primary">Primary</Button>
+<Button theme="success">Success</Button>
+<Button theme="info">Info</Button>`
+    }
+  }
+}
 export const Link = Template.bind({})
 addDescription(Link, 'Buttons with `href` props will be rendered as anchor tags.')
 Link.args = { href: 'https://creativecommons.org', target: '_blank' }
+Link.parameters = {
+  docs: { source: { code: `<Button href="https://creativecommons.org" target: "_blank">Button</Button>` } }
+}
 
 export const Text = Template.bind({})
 addDescription(Text, 'Text buttons lack background color or border.')
 Text.args = { theme: 'text' }
+Text.parameters = {
+  docs: { source: { code: `<Button theme="text">Button</Button>` } }
+}
