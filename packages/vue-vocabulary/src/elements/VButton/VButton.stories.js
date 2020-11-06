dd4a50e50ee8441435551506b7c1e7fe9c09b8c2
Olga Bulat <obulat@gmail.com>
2020-11-06 22:54:59 +0300

    Small refactoring of button templates

--- packages/vue-vocabulary/src/elements/VButton/VButton.stories.js
+++ packages/vue-vocabulary/src/elements/VButton/VButton.stories.js
@@ -25,20 +25,19 @@ const Template = (args, { argTypes }) => ({
 export const Default = Template.bind({})
 addSource(Default, `<VButton>Text</VButton>`)
 
-export const Theme = (args, { argTypes }) => ({
-  props: Object.keys(argTypes),
-  components: { VButton },
-  template: `<div style="display: flex; gap: 4px;">
+const themeTemplate = `<div style="display: flex; gap: 4px;">
     <VButton v-bind="$props" theme="primary">Primary</VButton>
     <VButton v-bind="$props" theme="success">Success</VButton>
     <VButton v-bind="$props" theme="info">Info</VButton>
   </div>`
+export const Theme = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { VButton },
+  template: themeTemplate
 })
 addDescription(Theme, 'Buttons can be styled with various colors. The primary button should be used for major site actions. There should rarely be more than one primary button per page.')
 Theme.args = {}
-addSource(Theme, `<VButton theme="primary">Primary</VButton>
-<VButton theme="success">Success</VButton>
-<VButton theme="info">Info</VButton>`)
+addSource(Theme, themeTemplate)
 
 export const Link = Template.bind({})
 addDescription(Link, 'Buttons with `href` props will be rendered as anchor tags.')
