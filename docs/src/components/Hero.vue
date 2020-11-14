e54e3af05d63b6307cd7d4960135d805ca49ded9
nimishbongale <nimishnb98@gmail.com>
2020-11-14 08:23:09 +0530

    The hero hath arrived! :rocket:

--- docs/src/components/Hero.vue
+++ docs/src/components/Hero.vue
@@ -6,31 +6,39 @@
 -->
 
 <template>
-    <div style="min-height: 40vmax;">
-      <div :id="id" class="has-background-white padding-bigger section-title">
+  <div style="hero">
+    <div :id="id" class="has-background-white padding-bigger section-title">
       <div class="container">
         <div class="content-wrap section-margin">
-          <div class="is-three-quarters">
-            <p class="title is-1 has-text-gray">WELCOME TO THE DESIGN SYSTEM OF CREATIVE COMMONS OPEN SOURCE.</p><br>
-            <p class="subtitle is-4" style="font-weight: normal; color: #767676;">{{ content }}</p><br>
-              <form action="https://cc-vocabulary.netlify.app" target="_blank">
-                <button class="button">VISIT DOCUMENTATION<center><br><i class="icon external-link"></i></center></button>
-              </form>
-              <br>
-              <form action="https://github.com/creativecommons/vocabulary" target="_blank">
-                <button class="button">VISIT GITHUB<center><br><i class="icon github"></i></center></button>
-              </form>
-            <center><p style="monospace"><code class="body-big margin-top-bigger is-inline-block">npm install @creativecommons/vocabulary</code></p></center>
+          <div class="title is-1 has-text-gray has-text-upper">Vocabulary is the <span style="color: #008B00;">design system</span> powering <span style="color: #F8CC2C;">Creative Commons</span>.</div><br>
+          <p class="subtitle is-4 sub-title spacer">Vocabulary is a cohesive design system and component library used on all Creative Commons web properties. The code is freely available to anyone and everyone to inspect, modify & use for their own purposes.</p>
+          <p class="monospace" style="margin-bottom: 3.5rem;"><code class="body-big margin-top-bigger is-inline-block">npm install @creativecommons/vocabulary</code></p>
+          <div class="group">
+            <form action="https://cc-vocabulary.netlify.app" target="_blank" class="separator spacer">
+              <button class="button is-success has-text-upper">Visit Documentation</button>
+            </form>
+            <form action="https://github.com/creativecommons/vocabulary" target="_blank">
+              <button class="button"><i class="icon github has-text-upper" style="margin-top:0.5rem; margin-right: 0.25rem;"></i>Visit GitHub</button>
+            </form>
           </div>
         </div>
       </div>
     </div>
-    </div>
+  </div>
 </template>
 
 <style scoped>
-  .section-title {
-    min-height: 20rem;
+  .hero {
+    min-height: 40vmax;
+  }
+
+  .sub-title {
+    font-weight: normal; 
+    color: #767676; 
+  }
+
+  .spacer {
+    margin-bottom: 1.5rem;
   }
 </style>
 
