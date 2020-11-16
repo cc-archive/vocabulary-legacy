d5fa756fb3e1cb058f602cbc460f85b25fbcb49e
nimishbongale <nimishnb98@gmail.com>
2020-11-16 16:48:36 +0530

    Changes to Hero 1

--- docs/src/components/Header.vue
+++ docs/src/components/Header.vue
@@ -8,44 +8,42 @@
 -->
 
 <template>
-  <div>
-    <header>
-      <nav class="navbar container">
-        <div class="navbar-brand">
-          <div class="has-text-black">
-            <img 
-              src="https://unpkg.com/@creativecommons/vocabulary@2020.9.4/assets/logos/products/vocabulary.svg"
-              alt="vocabulary logo"
-              width="200"
-              height="200"
-              style="margin-top: 0.25rem;"
-              />
-          </div>
-          <a role="button" class="navbar-burger is-active" aria-label="menu" aria-expanded="false" rel="noopener">
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
+  <header>
+    <nav class="navbar container">
+      <div class="navbar-brand">
+        <div class="has-text-black">
+          <img 
+            src="https://unpkg.com/@creativecommons/vocabulary@2020.9.4/assets/logos/products/vocabulary.svg"
+            alt="vocabulary logo"
+            width="200"
+            height="200"
+            style="margin-top: 0.25rem;"
+            />
+        </div>
+        <a role="button" class="navbar-burger is-active" aria-label="menu" aria-expanded="false" rel="noopener">
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div class="navbar-menu is-active">
+        <div class="navbar-end">
+          <a class="navbar-item" href="https://github.com/creativecommons/vocabulary/releases" rel="noopener">
+            Release Notes
+            <i class="icon external-link"></i>
+          </a>
+          <a class="navbar-item" href="https://cc-vocabulary.netlify.app" target="_blank" rel="noopener">
+            Documentation
+            <i class="icon external-link"></i>
+          </a>
+          <a class="navbar-item" href="https://github.com/creativecommons/vocabulary" target="_blank" rel="noopener">
+            GitHub
+            <i class="icon github"></i>
           </a>
         </div>
-        <div class="navbar-menu is-active">
-          <div class="navbar-end">
-            <a class="navbar-item" href="https://github.com/creativecommons/vocabulary/releases" rel="noopener">
-              Release Notes
-              <i class="icon external-link"></i>
-            </a>
-            <a class="navbar-item" href="https://cc-vocabulary.netlify.app" target="_blank" rel="noopener">
-              Documentation
-              <i class="icon external-link"></i>
-            </a>
-            <a class="navbar-item" href="https://github.com/creativecommons/vocabulary" target="_blank" rel="noopener">
-              GitHub
-              <i class="icon github"></i>
-            </a>
-          </div>
-        </div>
-      </nav>
-    </header>
-  </div>
+      </div>
+    </nav>
+  </header>
 </template>
 
 <script>
