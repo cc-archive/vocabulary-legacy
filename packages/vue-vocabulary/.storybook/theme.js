81709e3fdf46db82fe50f8faa845e8dddefa0dfa
nimishbongale <nimishnb98@gmail.com>
2020-11-24 01:56:38 +0530

    Add custom dark theme for vue-vocabulary

--- packages/vue-vocabulary/.storybook/theme.js
+++ packages/vue-vocabulary/.storybook/theme.js
@@ -1,33 +1,37 @@
-import { create } from "@storybook/theming";
+import { create } from '@storybook/theming'
 
-import vocabularySvg from "@creativecommons/vocabulary/assets/logos/products/vocabulary.svg";
+import vocabularySvg from '@creativecommons/vocabulary/assets/logos/products/vocabulary.svg';
+import vocabularySvgInverted from '../../vocabulary/src/assets/logos/products/vocabulary_inverted.svg';
 
-export default create({
-  // Inherit
-  base: "light",
+const base = {
+    colorSecondary: 'rgb(251, 119, 41)',
+    barSelectedColor: 'rgb(251, 119, 41)',
+    fontBase: '"Source Sans Pro", sans-serif',
+    fontCode: '"Fira Code", monospace',
+    brandTitle: 'Vue Vocabulary',
+    brandUrl: 'https://opensource.creativecommons.org/cc-vue-vocabulary'
+  }
 
-  // Color scheme
-  colorPrimary: "rgb(0, 0, 0)", // No known usage
-  colorSecondary: "rgb(251, 119, 41)", // Accent color
+export const light = create({
+    base: 'light',
+    colorPrimary: 'rgb(0, 0, 0)',
+    appBg: 'rgb(255, 255, 255)',
+    barTextColor: 'rgb(73, 80, 87)',
+    barBg: 'rgb(255, 255, 255)',
+    textColor: 'rgb(0, 0, 0)',
+    textInverseColor: 'rgb(255, 255, 255)',
+    brandImage: vocabularySvg,
+    ...base
+  })
 
-  // UI colors
-  appBg: "rgb(248, 249, 250)",
-
-  // Rows of controls
-  barTextColor: "rgb(73, 80, 87)",
-  barSelectedColor: "rgb(251, 119, 41)",
-  barBg: "rgb(255, 255, 255)",
-
-  // Typography
-  fontBase: '"Source Sans Pro", sans-serif',
-  fontCode: '"Fira Code", monospace',
-
-  // Text colors
-  textColor: "rgb(0, 0, 0)",
-  textInverseColor: "rgb(255, 255, 255)",
-
-  // Branding
-  brandTitle: "Vue Vocabulary",
-  brandUrl: "https://opensource.creativecommons.org/cc-vue-vocabulary",
-  brandImage: vocabularySvg,
-});
+export const dark = create({
+    base: 'dark',
+    colorPrimary: 'rgb(255, 255, 255)',
+    appBg: 'rgb(47, 47, 47)',
+    barTextColor: 'rgb(193, 193, 193)',
+    barBg: 'rgb(47, 47, 47)',
+    textColor: 'rgb(255, 255, 255)',
+    textInverseColor: 'rgb(0, 0, 0)',
+    brandImage: vocabularySvgInverted,
+    ...base
+  })
