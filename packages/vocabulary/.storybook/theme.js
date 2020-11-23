f065fbcf9ccc71c2595f320939101bc4e7ab92ee
nimishbongale <nimishnb98@gmail.com>
2020-11-24 01:44:16 +0530

    Add custom theme for vocabulary

--- packages/vocabulary/.storybook/theme.js
+++ packages/vocabulary/.storybook/theme.js
@@ -1,31 +1,37 @@
 import { create } from '@storybook/theming'
 
-export default create({
-  // Inherit
-  base: 'light',
+import vocabularySvg from '../src/assets/logos/products/vocabulary.svg'
+import vocabularySvgInverted from '../src/assets/logos/products/vocabulary_inverted.svg'
 
-  // Color scheme
-  colorPrimary: 'rgb(0, 0, 0)', // No known usage
-  colorSecondary: 'rgb(251, 119, 41)', // Accent color
+const base = {
+    colorSecondary: 'rgb(251, 119, 41)',
+    barSelectedColor: 'rgb(251, 119, 41)',
+    fontBase: '"Source Sans Pro", sans-serif',
+    fontCode: '"Fira Code", monospace',
+    brandTitle: 'Vocabulary',
+    brandUrl: 'https://opensource.creativecommons.org/cc-vocabulary'
+  }
 
-  // UI colors
-  appBg: 'rgb(248, 249, 250)',
+export const light = create({
+    base: 'light',
+    colorPrimary: 'rgb(0, 0, 0)',
+    appBg: 'rgb(255, 255, 255)',
+    barTextColor: 'rgb(73, 80, 87)',
+    barBg: 'rgb(255, 255, 255)',
+    textColor: 'rgb(0, 0, 0)',
+    textInverseColor: 'rgb(255, 255, 255)',
+    brandImage: vocabularySvg,
+    ...base
+  })
 
-  // Rows of controls
-  barTextColor: 'rgb(73, 80, 87)',
-  barSelectedColor: 'rgb(251, 119, 41)',
-  barBg: 'rgb(255, 255, 255)',
-
-  // Typography
-  fontBase: '"Source Sans Pro", sans-serif',
-  fontCode: '"JetBrains Mono", monospace',
-
-  // Text colors
-  textColor: 'rgb(0, 0, 0)',
-  textInverseColor: 'rgb(255, 255, 255)',
-
-  // Branding
-  brandTitle: 'Vocabulary',
-  brandUrl: 'https://opensource.creativecommons.org/cc-vocabulary',
-  brandImage: 'logos/products/vocabulary.svg#vocabulary'
-})
+export const dark = create({
+    base: 'dark',
+    colorPrimary: 'rgb(255, 255, 255)',
+    appBg: 'rgb(47, 47, 47)',
+    barTextColor: 'rgb(193, 193, 193)',
+    barBg: 'rgb(47, 47, 47)',
+    textColor: 'rgb(255, 255, 255)',
+    textInverseColor: 'rgb(0, 0, 0)',
+    brandImage: vocabularySvgInverted,
+    ...base
+  })
