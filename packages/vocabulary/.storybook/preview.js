4fdb0a933ccb4d9d744022e988af695d12386efc
nimishbongale <nimishnb98@gmail.com>
2020-11-24 01:43:04 +0530

    Add default dark theme for vocabulary

--- packages/vocabulary/.storybook/preview.js
+++ packages/vocabulary/.storybook/preview.js
@@ -6,21 +6,27 @@ import { withKnobs } from '@storybook/addon-knobs'
 import viewports from './viewport'
 import order from './order'
 
+import { light, dark } from './theme'
+
 import '../dist/css/vocabulary.css'
 
 addParameters({
   options: {
     showRoots: true,
-    storySort: order
-  },
-  backgrounds: [
-    { name: 'canvas', value: '#f5f5f5', default: true },
-    { name: 'white', value: '#ffffff' },
-    { name: 'black', value: '#000000' }
-  ],
-  viewport: {
-    viewports
-  }
+    storySort: order,
+    backgrounds: [
+        { name: 'canvas', value: '#f5f5f5', default: true },
+        { name: 'white', value: '#ffffff' },
+        { name: 'black', value: '#000000' }
+      ]
+    },
+    viewport: {
+      viewports
+    },
+    darkMode: {
+      light: light,
+      dark: dark
+    }
 })
 
 addDecorator(withDesign)
