d15c774e22e97d473f1f7fa6a1ee9ad8a973bbbc
nimishbongale <nimishnb98@gmail.com>
2020-11-24 01:19:43 +0530

    Use default addon

--- packages/fonts/.storybook/preview.js
+++ packages/fonts/.storybook/preview.js
@@ -1,6 +1,8 @@
-import { addDecorator, addParameters, } from '@storybook/html'
+import { addParameters } from '@storybook/html'
 
 import order from './order'
+
+import { light, dark } from './theme'
 
 import '../dist/css/fonts.css'
 import '../dist/css/accidenz_commons.css'
@@ -10,10 +12,13 @@ addParameters({
     showRoots: true,
     storySort: order,
     backgrounds: [
-      { name: 'canvas', value: '#f5f5f5', default: true },
-      { name: 'white', value: '#ffffff' },
-      { name: 'black', value: '#000000' }
-    ]
-  }
+        { name: 'canvas', value: '#f5f5f5', default: true },
+        { name: 'white', value: '#ffffff' },
+        { name: 'black', value: '#000000' }
+      ]
+    },
+    darkMode: {
+      light: light,
+      dark: dark
+    }
 })
-
