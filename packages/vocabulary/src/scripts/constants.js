714896113619e0298b771dfb67d067850d3ea007
Zack Krida <zackkrida@pm.me>
2020-11-25 11:50:44 -1000

    Switch classy links to cc.org/donate link

--- packages/vocabulary/src/scripts/constants.js
+++ packages/vocabulary/src/scripts/constants.js
@@ -1,12 +1,14 @@
-export const CC_ORG_URL = 'https://creativecommons.org'
-export const DONATION_URL = 'https://classy.org/give/313412/#!/donation/checkout?c_src=website&c_src2=GlobalHeader'
+export const CC_ORG_URL = "https://creativecommons.org";
+export const DONATION_URL =
+  "https://creativecommons.org/donate?c_src=website&c_src2=GlobalHeader";
 
-export const CDN_BASE_URL = 'https://unpkg.com/@creativecommons/vocabulary'
-export const GLOBAL_HEADER_API_URL = 'https://creativecommons.org/wp-json/ccglobal/menu'
+export const CDN_BASE_URL = "https://unpkg.com/@creativecommons/vocabulary";
+export const GLOBAL_HEADER_API_URL =
+  "https://creativecommons.org/wp-json/ccglobal/menu";
 
 // TODO: Internationalise
-export const DONATION_TITLE = 'Our work relies on you!'
-export const DONATION_DESCRIPTION = 'Help us keep the internet free and open.'
-export const DONATION_BUTTON_TEXT = 'Donate now'
-export const NAVIGATION_TAB_TEXT = 'Explore CC'
-export const VISIT_SITE_BUTTON_TEXT = 'Visit site'
+export const DONATION_TITLE = "Our work relies on you!";
+export const DONATION_DESCRIPTION = "Help us keep the internet free and open.";
+export const DONATION_BUTTON_TEXT = "Donate now";
+export const NAVIGATION_TAB_TEXT = "Explore CC";
+export const VISIT_SITE_BUTTON_TEXT = "Visit site";
