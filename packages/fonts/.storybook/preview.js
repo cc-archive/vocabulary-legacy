f9bac8a2d215925966c33212458c90da73c05d4c
Zack Krida <zackkrida@pm.me>
2020-12-04 14:52:21 -1000

    Remove shared package :sad:

--- packages/fonts/.storybook/preview.js
+++ packages/fonts/.storybook/preview.js
@@ -9,8 +9,43 @@ import fontsSvgInverted from '../src/assets/logos/fonts_inverted.svg'
 import '../dist/css/fonts.css'
 import '../dist/css/accidenz_commons.css'
 
-import { light, dark } from '../../shared/theme'
-import { backgrounds } from '../../shared/backgrounds'
+const base = {
+  colorSecondary: "rgb(251, 119, 41)",
+  barSelectedColor: "rgb(251, 119, 41)",
+  fontBase: '"Source Sans Pro", sans-serif',
+  fontCode: '"Fira Code", monospace',
+};
+
+const light = {
+  base: "light",
+  colorPrimary: "rgb(0, 0, 0)",
+  appBg: "rgb(255, 255, 255)",
+  barTextColor: "rgb(73, 80, 87)",
+  barBg: "rgb(255, 255, 255)",
+  textColor: "rgb(0, 0, 0)",
+  textInverseColor: "rgb(255, 255, 255)",
+  ...base,
+};
+
+const dark = {
+  base: "dark",
+  colorPrimary: "rgb(255, 255, 255)",
+  appBg: "rgb(47, 47, 47)",
+  barTextColor: "rgb(193, 193, 193)",
+  barBg: "rgb(47, 47, 47)",
+  textColor: "rgb(255, 255, 255)",
+  textInverseColor: "rgb(0, 0, 0)",
+  ...base,
+};
+
+const backgrounds = {
+  default: "canvas",
+  values: [
+    { name: "canvas", value: "#f5f5f5" },
+    { name: "white", value: "#ffffff" },
+    { name: "black", value: "#000000" },
+  ],
+};
 
 const meta = {
   brandTitle: 'Fonts',
