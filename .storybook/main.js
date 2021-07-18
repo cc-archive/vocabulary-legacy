7c1c636318b7f8bd5af5115bc9bd688261dcf4de
Dhruv Bhanushali <dhruv_b@live.com>
2021-07-18 21:57:57 +0530

    Setup SCSS loader

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,7 @@ module.exports = {
     '../src/**/*.stories.@(mdx|js|jsx|ts|tsx)',
   ],
   'addons': [
+    '@storybook/preset-scss',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
   ],
