0e236cea86543a94e435ae31045df9e480c1aee7
Dhruv Bhanushali <dhruv_b@live.com>
2021-07-21 18:01:23 +0530

    Reorganise files from Vocabulary into the new structure

--- src/fonts.js
+++ src/fonts.js
@@ -1,7 +1,3 @@
 // This is the entrypoint for building Fonts.
 
-// Sass stylesheets, will be compiled
-import '@/styles/fonts/accidenz_commons.scss';
-import '@/styles/fonts/jetbrains_mono.scss';
-import '@/styles/fonts/roboto_condensed.scss';
-import '@/styles/fonts/source_sans_pro.scss';
+import '@/styles/fonts/_index.scss'; // Sass stylesheets, will be compiled
