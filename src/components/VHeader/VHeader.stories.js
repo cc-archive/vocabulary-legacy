8b425d23412a15a12d93f2929aa5fc2aab02db03
Anik Das <anikdas0811@gmail.com>
2021-09-09 15:55:38 +0530

    fix: VHeader story and css fixed
    header.scss L#1 also needs review if it actually was the bug or not
    
    Signed-off-by: Anik Das <anikdas0811@gmail.com>

---
+++ src/components/VHeader/VHeader.stories.js
@@ -0,0 +1,56 @@
+import VHeader from './VHeader.vue';
+import NavItem from './NavItem.vue';
+import NavDropdown from './NavDropdown.vue';
+import ChooserLogo from './ChooserLogo.vue';
+import { addDescription } from '../../utils/addDescription';
+
+export default {
+  title: 'Vue/Elements/VHeader',
+  component: VHeader,
+};
+
+const menuItems = `
+<NavItem tag="a" href="/whatever" label="Item One" />
+<NavItem tag="a" href="/whatever" label="Item External" :isExternal="true" />
+<NavDropdown label="Item Three">
+<NavItem tag="a" href="/whatever" label="Item Three A" />
+<NavItem tag="a" href="/whatever" label="Item Three B" />
+</NavDropdown>`;
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { VHeader, NavItem, NavDropdown },
+  template: `<VHeader v-bind="$props"><template #menu-items>${menuItems}</template></VHeader>`,
+});
+
+export const Default = Template.bind({});
+
+export const ChooserHeader = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { VHeader, NavItem, ChooserLogo },
+  template: `<VHeader v-bind="$props">
+<template #logo><ChooserLogo /></template>
+<template #menu-items>
+<NavItem tag="a" href="https://docs.google.com/forms/d/e/1FAIpQLSfF7MCKxlPsPuMn17v_sLYWMkxBkudQSPXCXoJKjh5GCtx63g/viewform" label="Feedback" :isExternal="true" />
+</template></VHeader>`,
+});
+addDescription(
+  ChooserHeader,
+  'This is a sample header for the Chooser page. It uses `<ChooserLogo />` svg component as the logo, and one `<NavItem>` component.',
+);
+
+export const IntlHeader = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { VHeader, NavItem, ChooserLogo },
+  template: `<VHeader v-bind="$props">
+<template #menu-items>
+<NavItem tag="a" href="/menu_item" label="Меню 1" />
+<NavItem tag="a" href="/menu_item" label="Меню 2" :isExternal="true" />
+</template></VHeader>`,
+});
+IntlHeader.args = { ariaPrimaryLabel: 'Основная', ariaMenuLabel: 'Меню' };
+
+addDescription(
+  IntlHeader,
+  'When using i18n, you should pass translated strings to the header. Header has two aria labels that need to be bound to it if the language is not English.',
+);
