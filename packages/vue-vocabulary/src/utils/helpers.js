b3bbe0bcb71645165e848a91c9b8d6c5b78c3b10
Anik Das <anikdas0811@gmail.com>
2021-09-17 23:06:25 +0530

    chore: deleted packages/
    all components are ported to the new vue-cli part
    
    Signed-off-by: Anik Das <anikdas0811@gmail.com>

--- packages/vue-vocabulary/src/utils/helpers.js
+++
@@ -1,6 +0,0 @@
-import { config } from 'storybook-addon-designs'
-
-export const figmaConfig = (nodeId) => config({
-  type: 'figma',
-  url: `https://www.figma.com/file/l4Mt3dn3Ndtrvrb4aLcwXI/Design-Library?node-id=${nodeId}`
-})
