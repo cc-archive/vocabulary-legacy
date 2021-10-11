47b97e97cfdb5277ddbc333b70bf8bbea1e5844d
Muluh MG Godson <godsonmuluh@gmail.com>
2021-10-11 06:47:11 -0500

    add docker file

---
+++ docker/vocabulary/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run \
+  --interactive --tty \
+  --volume node_modules:/codebase/node_modules \
+  --volume "$(pwd)":/codebase \
+  --net host \
+  --name vocabulary-cmd \
+  vocabulary:latest \
+  bash
