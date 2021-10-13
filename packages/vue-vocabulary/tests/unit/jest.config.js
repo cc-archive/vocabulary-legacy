6d45f173bdd344a03ac303ce7298c928c35fe94a
Muluh MG Godson <godsonmuluh@gmail.com>
2021-10-13 16:44:11 +0100

    added test for @vue-vocabulary package

---
+++ packages/vue-vocabulary/tests/unit/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'VUE-VOCABULARY',
+  preset: '@vue/cli-plugin-unit-jest',
+  moduleFileExtensions: [
+    'js',
+    'jsx',
+    'json',
+    'vue',
+  ],
+  testMatch: [
+    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
+  ],
+
+};
